a59ed3cded91c10468196afcad2d81d5df427dce
FooFieOwO <foofieowo@gmail.com>
2023-07-06 23:35:28 +0200

    penis

--- src/main/java/me/nekosarekawaii/foxglove/config/impl/AccountConfig.java
+++ src/main/java/me/nekosarekawaii/foxglove/config/impl/AccountConfig.java
@@ -9,6 +9,7 @@ import me.nekosarekawaii.foxglove.config.ValueableConfig;
 import me.nekosarekawaii.foxglove.gui.imgui.impl.alt.alttype.Account;
 import me.nekosarekawaii.foxglove.gui.imgui.impl.alt.alttype.type.CrackedAccount;
 import me.nekosarekawaii.foxglove.gui.imgui.impl.alt.alttype.type.MicrosoftAccount;
+import net.minecraft.client.MinecraftClient;
 
 import java.io.IOException;
 
@@ -34,6 +35,7 @@ public class AccountConfig extends ValueableConfig {
             accountArray.add(accountObject);
         }
 
+        configObject.addProperty("lastSession", MinecraftClient.getInstance().session.getUsername());
         configObject.add("alts", accountArray);
         return configObject;
     }
@@ -46,6 +48,7 @@ public class AccountConfig extends ValueableConfig {
             final JsonObject accountObject = accountElement.getAsJsonObject();
             final String username = accountObject.get("username").getAsString();
             final String type = accountObject.get("type").getAsString();
+            final String lastSession = jsonObject.get("lastSession").getAsString();
 
             switch (type) {
                 case "microsoft" -> {
@@ -53,15 +56,29 @@ public class AccountConfig extends ValueableConfig {
                     final String password = accountObject.get("password").getAsString();
                     final String refreshToken = accountObject.get("refreshToken").getAsString();
                     final String uuid = accountObject.get("uuid").getAsString();
+                    final MicrosoftAccount microsoftAccount;
 
                     if (refreshToken.isEmpty() || uuid.isEmpty()) {
-                        this.accounts.add(new MicrosoftAccount(email, password));
+                        microsoftAccount = new MicrosoftAccount(email, password);
                     } else {
-                        this.accounts.add(new MicrosoftAccount(email, password, refreshToken, uuid, username));
+                        microsoftAccount = new MicrosoftAccount(email, password, refreshToken, uuid, username);
+                    }
+
+                    this.accounts.add(microsoftAccount);
+
+                    if (lastSession.equals(microsoftAccount.getUsername())) {
+                        microsoftAccount.login();
                     }
                 }
 
-                default -> this.accounts.add(new CrackedAccount(username));
+                default -> {
+                    final CrackedAccount crackedAccount = new CrackedAccount(username);
+                    this.accounts.add(crackedAccount);
+
+                    if (lastSession.equals(crackedAccount.getUsername())) {
+                        crackedAccount.login();
+                    }
+                }
             }
         }
     }
