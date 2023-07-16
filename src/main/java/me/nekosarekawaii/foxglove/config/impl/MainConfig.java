fb0593d49ca54ca16c72bce7627378170bd3eee1
FooFieOwO <51881617+FooFieOwO@users.noreply.github.com>
2023-07-16 15:10:09 +0200

    a (#33)
    * that aint work
    
    * fixed tab colors.
    
    * added stuff to ColorUtils
    
    * cleanup settings

--- src/main/java/me/nekosarekawaii/foxglove/config/impl/MainConfig.java
+++ src/main/java/me/nekosarekawaii/foxglove/config/impl/MainConfig.java
@@ -5,6 +5,7 @@ import it.unimi.dsi.fastutil.Pair;
 import me.nekosarekawaii.foxglove.Foxglove;
 import me.nekosarekawaii.foxglove.config.ValueableConfig;
 import me.nekosarekawaii.foxglove.value.Value;
+import me.nekosarekawaii.foxglove.value.ValueCategory;
 import me.nekosarekawaii.foxglove.value.values.BooleanValue;
 import me.nekosarekawaii.foxglove.value.values.KeyInputValue;
 import me.nekosarekawaii.foxglove.value.values.StringValue;
@@ -15,94 +16,100 @@ import java.io.IOException;
 
 public class MainConfig extends ValueableConfig {
 
+    public final ValueCategory menuCategory = new ValueCategory("Menu", "Menu Settings", this);
+
     public final Value<Pair<Integer, String>> menuBarKey = new KeyInputValue(
             "Menu Bar Key",
             "Change the key to open the Menu Bar.",
-            this,
+            menuCategory,
             GLFW.GLFW_KEY_MENU,
             "menu"
     );
 
-    public final Value<String> commandPrefix = new StringValue(
-            "Command Prefix",
-            "Change the prefix to run the commands of the Mod.",
-            this,
-            "."
+    public final Value<Boolean> manageContainerButtons = new BooleanValue(
+            "Manage Container Buttons",
+            "Adds the store/steal buttons to containers.",
+            menuCategory,
+            true
     );
 
     public final Value<Boolean> multiplayerScreenServerInformation = new BooleanValue(
             "Multiplayer Screen Server Information",
             "If enabled the Game shows all necessary server information behind a server list entry.",
-            this,
+            menuCategory,
             true
     );
 
     public final Value<Integer> maxServerVersionLength = new SliderIntegerValue(
             "Max Server Version Length",
             "Sets the max display length of a server version that is being displayed in the multiplayer screen.",
-            this,
+            menuCategory,
             60,
             6,
             250
     ).visibleConsumer(this.multiplayerScreenServerInformation::getValue);
 
+    public final ValueCategory chatCategory = new ValueCategory("Chat", "Chat settings", this);
+
+    public final Value<String> commandPrefix = new StringValue(
+            "Command Prefix",
+            "Change the prefix to run the commands of the Mod.",
+            chatCategory,
+            "."
+    );
+
     public final Value<Boolean> allowColorChar = new BooleanValue(
             "Allow Color Char",
             "Disables the color char restrictions of the Game.",
-            this,
-            true
-    );
-
-    public final Value<Boolean> manageContainerButtons = new BooleanValue(
-            "Manage Container Buttons",
-            "Adds the store/steal buttons to containers.",
-            this,
+            chatCategory,
             true
     );
 
     public final Value<Boolean> dontClearChatHistory = new BooleanValue(
             "Dont Clear Chat History",
             "Prevents the Game from clearing your chat history.",
-            this,
-            true
-    );
-
-    public final Value<Boolean> antiTelemetry = new BooleanValue(
-            "Anti Telemetry",
-            "Blocks the Telemetry of the Game.",
-            this,
-            true
-    );
-
-    public final Value<Boolean> antiServerBlockList = new BooleanValue(
-            "Anti Server Block List",
-            "Blocks the Server Block List from the Game.",
-            this,
-            true
-    );
-
-    public final Value<Boolean> antiTimeoutKick = new BooleanValue(
-            "Anti Timeout Kick",
-            "Prevents the Game from disconnecting after 30 seconds if the server doesn't response.",
-            this,
+            chatCategory,
             true
     );
 
     public final Value<Boolean> customChatLength = new BooleanValue(
             "Custom Chat Length",
             "Allows you to enable or disable a custom chat length.",
-            this,
+            chatCategory,
             true
     );
 
     public final Value<Integer> maxChatLength = new SliderIntegerValue(
             "Max Chat Length",
             "Set the Max Chat Length",
-            this,
+            chatCategory,
             1000,
             1,
             10000
     ).visibleConsumer(this.customChatLength::getValue);
+
+    public final ValueCategory accessibilityCategory = new ValueCategory("Accessibility", "Accessibility settings", this);
+
+    public final Value<Boolean> antiTelemetry = new BooleanValue(
+            "Anti Telemetry",
+            "Blocks the Telemetry of the Game.",
+            accessibilityCategory,
+            true
+    );
+
+    public final Value<Boolean> antiServerBlockList = new BooleanValue(
+            "Anti Server Block List",
+            "Blocks the Server Block List from the Game.",
+            accessibilityCategory,
+            true
+    );
+
+    public final Value<Boolean> antiTimeoutKick = new BooleanValue(
+            "Anti Timeout Kick",
+            "Prevents the Game from disconnecting after 30 seconds if the server doesn't response.",
+            accessibilityCategory,
+            true
+    );
 
     public MainConfig() {
         super(Foxglove.getInstance().getDir(), "main");
--- src/main/java/me/nekosarekawaii/foxglove/util/render/ColorUtils.java
+++ src/main/java/me/nekosarekawaii/foxglove/util/render/ColorUtils.java
@@ -12,7 +12,7 @@ public class ColorUtils {
         final int bluePart = (int) (color1.getBlue() * percent + color2.getBlue() * inverse_percent);
         final int alphaPart = (int) (color1.getAlpha() * percent + color2.getAlpha() * inverse_percent);
 
-        return rgbToValue(redPart, greenPart, bluePart, alphaPart);
+        return rgbaToValue(redPart, greenPart, bluePart, alphaPart);
     }
 
     public static int interpolate(final Color color1, final Color color2, final Color color3, final double percent) {
@@ -22,11 +22,30 @@ public class ColorUtils {
         return interpolate(color1, color2, (percent - 0.5) * 2D);
     }
 
-    public static int rgbToValue(final int r, final int g, final int b, final int a) {
+    public static int interpolate(final Color color1, final Color color2, final Color color3, final Color color4, final double percent) {
+        if (percent <= 0.33)
+            return interpolate(color2, color3, color4, percent * 3D);
+
+        if (percent <= 0.66)
+            return interpolate(color1, color2, color3, (percent - 0.33) * 3D);
+
+        return interpolate(color1, color2, (percent - 0.66) * 3D);
+    }
+
+    public static int rgbaToValue(final int r, final int g, final int b, final int a) {
         return ((a & 0xFF) << 24) |
                 ((r & 0xFF) << 16) |
                 ((g & 0xFF) << 8) |
                 ((b & 0xFF));
+    }
+
+    public static int[] valueToRGBA(final int value) {
+        return new int[]{
+                ((value >> 16) & 0xFF),
+                ((value >> 8) & 0xFF),
+                (value & 0xFF),
+                ((value >> 24) & 0xFF)
+        };
     }
 
 }
