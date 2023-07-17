8fd6dc1c902169c384abeabedc0afce72438eddc
NekosAreKawaii <nekos.are.really.cute@gmail.com>
2023-07-17 17:50:42 +0200

    More Grief Items.

--- src/main/java/me/nekosarekawaii/foxglove/creativetab/impl/GriefItemsCreativeTab.java
+++ src/main/java/me/nekosarekawaii/foxglove/creativetab/impl/GriefItemsCreativeTab.java
@@ -19,11 +19,12 @@ public class GriefItemsCreativeTab extends CreativeTab {
     public Collection<ItemStack> entries() {
         final Collection<ItemStack> current = super.entries();
 
-        final ItemStack griefCreeperSpawnEgg = new ItemStack(Items.CREEPER_SPAWN_EGG);
+        final ItemStack griefCreeperSpawnEgg = new ItemStack(Items.PIG_SPAWN_EGG);
         final NbtCompound griefCreeperSpawnEggNBT = new NbtCompound();
         final NbtCompound griefCreeperSpawnEggEntityTag = new NbtCompound();
         griefCreeperSpawnEggEntityTag.putInt("Fuse", 0);
         griefCreeperSpawnEggEntityTag.putInt("ExplosionRadius", 127);
+        griefCreeperSpawnEggEntityTag.putString("id", "minecraft:creeper");
         griefCreeperSpawnEggNBT.put("EntityTag", griefCreeperSpawnEggEntityTag);
         griefCreeperSpawnEgg.setNbt(griefCreeperSpawnEggNBT);
         this.putClientsideName(griefCreeperSpawnEgg,
@@ -33,22 +34,49 @@ public class GriefItemsCreativeTab extends CreativeTab {
         );
         current.add(griefCreeperSpawnEgg);
 
-        final ItemStack enchantedGriefCreeperSpawnEgg = new ItemStack(Items.CREEPER_SPAWN_EGG);
+        final ItemStack enchantedGriefCreeperSpawnEgg = new ItemStack(Items.COW_SPAWN_EGG);
         final NbtCompound enchantedGriefCreeperSpawnEggNBT = new NbtCompound();
         final NbtCompound enchantedGriefCreeperSpawnEggEntityTag = new NbtCompound();
         enchantedGriefCreeperSpawnEggEntityTag.putInt("Fuse", 0);
         enchantedGriefCreeperSpawnEggEntityTag.putInt("ExplosionRadius", 127);
         enchantedGriefCreeperSpawnEggEntityTag.putByte("powered", (byte) 1);
+        enchantedGriefCreeperSpawnEggEntityTag.putString("id", "minecraft:creeper");
         enchantedGriefCreeperSpawnEggNBT.put("EntityTag", enchantedGriefCreeperSpawnEggEntityTag);
         enchantedGriefCreeperSpawnEgg.setNbt(enchantedGriefCreeperSpawnEggNBT);
         this.putClientsideGlint(enchantedGriefCreeperSpawnEgg);
         this.putClientsideName(enchantedGriefCreeperSpawnEgg,
                 Text.literal(
-                        Formatting.DARK_PURPLE + Formatting.BOLD.toString() + "Powered " +
+                        Formatting.GOLD + Formatting.BOLD.toString() + "Powered " +
                                 Formatting.RED + Formatting.BOLD + "Grief Creeper"
                 )
         );
         current.add(enchantedGriefCreeperSpawnEgg);
+
+        final ItemStack enderDragonSpawnEgg = new ItemStack(Items.FOX_SPAWN_EGG);
+        final NbtCompound enderDragonSpawnEggNBT = new NbtCompound();
+        final NbtCompound enderDragonSpawnEggEntityTag = new NbtCompound();
+        enderDragonSpawnEggEntityTag.putString("id", "minecraft:ender_dragon");
+        enderDragonSpawnEggNBT.put("EntityTag", enderDragonSpawnEggEntityTag);
+        enderDragonSpawnEgg.setNbt(enderDragonSpawnEggNBT);
+        this.putClientsideName(enderDragonSpawnEgg,
+                Text.literal(
+                        Formatting.LIGHT_PURPLE + Formatting.BOLD.toString() + "Ender Dragon"
+                )
+        );
+        current.add(enderDragonSpawnEgg);
+
+        final ItemStack witherSpawnEgg = new ItemStack(Items.AXOLOTL_SPAWN_EGG);
+        final NbtCompound witherSpawnEggNBT = new NbtCompound();
+        final NbtCompound witherSpawnEggEntityTag = new NbtCompound();
+        witherSpawnEggEntityTag.putString("id", "minecraft:wither");
+        witherSpawnEggNBT.put("EntityTag", witherSpawnEggEntityTag);
+        witherSpawnEgg.setNbt(witherSpawnEggNBT);
+        this.putClientsideName(witherSpawnEgg,
+                Text.literal(
+                        Formatting.WHITE + Formatting.BOLD.toString() + "Wither"
+                )
+        );
+        current.add(witherSpawnEgg);
 
         return current;
     }
