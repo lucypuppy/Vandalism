2a02da18e5893b5b713cb8109fd77fbd808e12b9
NekosAreKawaii <nekos.are.really.cute@gmail.com>
2023-07-19 17:14:39 +0200

    Fixed löng display names. Fixed skill issue with imgui and chatscreen.

--- src/main/java/me/nekosarekawaii/foxglove/mixin/imgui/glfw/MixinImGuiImplGlfw.java
+++ src/main/java/me/nekosarekawaii/foxglove/mixin/imgui/glfw/MixinImGuiImplGlfw.java
@@ -2,17 +2,18 @@ package me.nekosarekawaii.foxglove.mixin.imgui.glfw;
 
 import imgui.glfw.ImGuiImplGlfw;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ChatScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ImGuiImplGlfw.class)
-public class ImGuiImplGlfwMixin {
+public abstract class MixinImGuiImplGlfw {
 
     @Inject(method = "keyCallback", at = @At(value = "INVOKE", target = "Limgui/ImGui;getIO()Limgui/ImGuiIO;", shift = At.Shift.AFTER), cancellable = true, remap = false)
     public void injectKeyCallback(final long windowId, final int key, final int scancode, final int action, final int mods, final CallbackInfo ci) {
-        if (MinecraftClient.getInstance().mouse.isCursorLocked())
+        if (MinecraftClient.getInstance().mouse.isCursorLocked() || MinecraftClient.getInstance().currentScreen instanceof ChatScreen)
             ci.cancel();
     }
 
