2b36e917fa84f78f183cc6a782fbb446f696352d
FooFieOwO <51881617+FooFieOwO@users.noreply.github.com>
2023-07-21 04:25:39 +0200

    < (#40)
    * that aint work
    
    * fixed tab colors.
    
    * added stuff to ColorUtils
    
    * cleanup settings
    
    * cleanup module list and added hide hud setting.
    
    * fixed imgui.
    
    * fixed imgui.
    
    * fixed fontrenderer and keystuff with imgui
    
    * fixed eventmanager and account login.
    
    * fixed ColorValue
    
    * added better way to implement FastPlaceModule
    
    * fixed a few qol things.

--- src/main/java/me/nekosarekawaii/foxglove/feature/impl/module/impl/development/DebugModule.java
+++ src/main/java/me/nekosarekawaii/foxglove/feature/impl/module/impl/development/DebugModule.java
@@ -2,10 +2,8 @@ package me.nekosarekawaii.foxglove.feature.impl.module.impl.development;
 
 import de.florianmichael.dietrichevents2.DietrichEvents2;
 import imgui.ImGui;
-import imgui.extension.implot.ImPlot;
 import me.nekosarekawaii.foxglove.Foxglove;
 import me.nekosarekawaii.foxglove.event.impl.Render2DListener;
-import me.nekosarekawaii.foxglove.event.impl.TickListener;
 import me.nekosarekawaii.foxglove.feature.FeatureCategory;
 import me.nekosarekawaii.foxglove.feature.impl.module.Module;
 import me.nekosarekawaii.foxglove.feature.impl.module.ModuleInfo;
@@ -18,54 +16,24 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.Ownable;
 import net.minecraft.screen.ScreenHandler;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @ModuleInfo(name = "Debug Module", description = "Debug some stuff about the client.", category = FeatureCategory.DEVELOPMENT)
-public class DebugModule extends Module implements Render2DListener, TickListener {
+public class DebugModule extends Module implements Render2DListener {
 
     @Override
     protected void onEnable() {
         DietrichEvents2.global().subscribe(Render2DEvent.ID, this);
-        DietrichEvents2.global().subscribe(TickEvent.ID, this);
     }
 
     @Override
     protected void onDisable() {
         DietrichEvents2.global().unsubscribe(Render2DEvent.ID, this);
-        DietrichEvents2.global().unsubscribe(TickEvent.ID, this);
-    }
-
-    private final List<Integer> fpsHistory = new ArrayList<>();
-    private final Integer[] graphSize = new Integer[100];
-    private int lastFPS = 0;
-
-    public DebugModule() {
-        for (int i = 0; i < this.graphSize.length; i++) {
-            this.graphSize[i] = i;
-            this.fpsHistory.add(0);
-        }
     }
 
     @Override
     public void onRender2DInGame(final DrawContext context, final float delta, final Window window) {
-        if (fpsHistory.size() < 100)
-            return;
-
         Foxglove.getInstance().getImGuiHandler().getImGuiRenderer().addRenderInterface(io -> {
-
-            if (ImGui.begin("Debug Graph", ImGuiUtil.getInGameFlags(0))) {
-                if (ImPlot.beginPlot("Debug Graph")) {
-                    ImPlot.plotLine("FPS", this.graphSize, this.fpsHistory.toArray(Integer[]::new));
-                    ImPlot.endPlot();
-                }
-
-                ImGui.end();
-            }
-
             final ClientPlayerEntity player = mc.player;
             if (player != null) {
-
                 final ScreenHandler screenHandler = player.currentScreenHandler;
                 if (screenHandler != null) {
                     if (ImGui.begin("Current Screen Handler", ImGuiUtil.getInGameFlags(0))) {
@@ -77,12 +45,13 @@ public class DebugModule extends Module implements Render2DListener, TickListene
 
                 final ClientWorld world = mc.world;
                 if (world != null) {
-
                     if (ImGui.begin("Entities in Range##debugModuleEntitiesInRange")) {
+
                         int i = 0;
                         for (final Entity entity : world.getEntities()) {
                             final String entityUUID = entity.getUuidAsString();
                             ImGui.text(entity.getName().getString() + " [" + entity.getClass().getSimpleName() + "] (" + player.distanceTo(entity) + ")");
+
                             if (entity instanceof final Ownable ownableEntity) {
                                 final Entity owner = ownableEntity.getOwner();
                                 if (owner != null) {
@@ -95,35 +64,20 @@ public class DebugModule extends Module implements Render2DListener, TickListene
                                     }
                                 }
                             }
+
                             ImGui.sameLine();
+
                             if (ImGui.button("Copy Entity UUID##copyEntityUUID" + i)) {
                                 mc.keyboard.setClipboard(entityUUID);
                             }
                             i++;
                         }
+
                         ImGui.end();
                     }
-
                 }
-
             }
-
         });
-    }
-
-    @Override
-    public void onTick() {
-        final int curFPS = mc.getCurrentFps();
-
-        if (curFPS != lastFPS) {
-            this.fpsHistory.add(curFPS);
-
-            if (this.fpsHistory.size() > 100) {
-                this.fpsHistory.remove(0);
-            }
-        }
-
-        this.lastFPS = curFPS;
     }
 
 }
