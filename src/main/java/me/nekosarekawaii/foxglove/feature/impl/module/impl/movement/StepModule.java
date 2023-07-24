0f687b2c865e5d912f285703f1a5ac2fae9a4074
NekosAreKawaii <nekos.are.really.cute@gmail.com>
2023-07-24 18:19:37 +0200

    funny hehe

--- src/main/java/me/nekosarekawaii/foxglove/feature/impl/module/impl/movement/StepModule.java
+++ src/main/java/me/nekosarekawaii/foxglove/feature/impl/module/impl/movement/StepModule.java
@@ -5,9 +5,20 @@ import me.nekosarekawaii.foxglove.event.impl.StepListener;
 import me.nekosarekawaii.foxglove.feature.FeatureCategory;
 import me.nekosarekawaii.foxglove.feature.impl.module.Module;
 import me.nekosarekawaii.foxglove.feature.impl.module.ModuleInfo;
+import me.nekosarekawaii.foxglove.value.Value;
+import me.nekosarekawaii.foxglove.value.values.number.slider.SliderFloatValue;
 
 @ModuleInfo(name = "Step", description = "Makes you step up blocks.", category = FeatureCategory.MOVEMENT)
 public class StepModule extends Module implements StepListener {
+
+    private final Value<Float> stepHeight = new SliderFloatValue(
+            "Step Height",
+            "Allows you to customize the step height.",
+            this,
+            10.0f,
+            0.7f,
+            10.0f
+    );
 
     @Override
     protected void onEnable() {
@@ -21,7 +32,7 @@ public class StepModule extends Module implements StepListener {
 
     @Override
     public void onStep(final StepEvent event) {
-        event.stepHeight = 10.0f;
+        event.stepHeight = this.stepHeight.getValue();
     }
 
 }
