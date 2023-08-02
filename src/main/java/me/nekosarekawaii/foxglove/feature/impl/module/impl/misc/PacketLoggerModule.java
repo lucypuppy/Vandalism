85f1f4300c89691f1897ed34fe30e717e033e09b
NekosAreKawaii <nekos.are.really.cute@gmail.com>
2023-08-02 16:58:04 +0200

    Added Open Screen S2C Packet to Packet Logger Module.

--- src/main/java/me/nekosarekawaii/foxglove/feature/impl/module/impl/misc/PacketLoggerModule.java
+++ src/main/java/me/nekosarekawaii/foxglove/feature/impl/module/impl/misc/PacketLoggerModule.java
@@ -7,21 +7,24 @@ import me.nekosarekawaii.foxglove.feature.FeatureCategory;
 import me.nekosarekawaii.foxglove.feature.impl.module.Module;
 import me.nekosarekawaii.foxglove.feature.impl.module.ModuleInfo;
 import me.nekosarekawaii.foxglove.util.minecraft.ChatUtils;
+import me.nekosarekawaii.foxglove.util.minecraft.inventory.ScreenHandlerTypes;
 import me.nekosarekawaii.foxglove.value.Value;
 import me.nekosarekawaii.foxglove.value.values.BooleanValue;
+import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
 import net.minecraft.network.packet.c2s.play.RequestCommandCompletionsC2SPacket;
 import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
+import net.minecraft.network.packet.s2c.play.OpenScreenS2CPacket;
 import net.minecraft.util.Identifier;
 
 @ModuleInfo(name = "Packet Logger", description = "Logs incoming and outgoing packets into the Chat.", category = FeatureCategory.MISC)
 public class PacketLoggerModule extends Module implements PacketListener {
 
-    private final Value<Boolean> serverToClient = new BooleanValue("S2C", "Enable / Disable the logging of incoming packets.", this, true);
-
     private final Value<Boolean> customPayloadPacket = new BooleanValue("Custom Payload Packet", "Logs custom payload packets into the Chat.", this, false);
 
-    private final Value<Boolean> requestCommandCompletionsPacket = new BooleanValue("Request Command Completions Packet", "Logs request command completions packet into the Chat.", this, false);
+    private final Value<Boolean> requestCommandCompletionsC2SPacket = new BooleanValue("Request Command Completions C2S Packet", "Logs request command completions packets into the Chat.", this, false);
+
+    private final Value<Boolean> openScreenS2CPacket = new BooleanValue("Open Screen S2C Packet", "Logs open screen packets into the Chat.", this, false);
 
     @Override
     protected void onEnable() {
@@ -35,27 +38,32 @@ public class PacketLoggerModule extends Module implements PacketListener {
 
     @Override
     public void onWrite(final PacketEvent event) {
-        if (event.packet instanceof final CustomPayloadC2SPacket customPayloadC2SPacket) {
+        final Packet<?> packet = event.packet;
+        if (packet instanceof final CustomPayloadC2SPacket c2SPacket) {
             if (this.customPayloadPacket.getValue()) {
-                final Identifier channel = customPayloadC2SPacket.getChannel();
+                final Identifier channel = c2SPacket.getChannel();
                 final String channelName = channel.getNamespace(), channelPath = channel.getPath();
                 ChatUtils.infoChatMessage("Outgoing custom payload packet > Channel Name: " + channelName + " | Channel Path: " + channelPath);
             }
-        } else if (event.packet instanceof final RequestCommandCompletionsC2SPacket requestCommandCompletionsC2SPacket) {
-            if (this.requestCommandCompletionsPacket.getValue()) {
-                ChatUtils.infoChatMessage("Outgoing request command completions packet > ID: " + requestCommandCompletionsC2SPacket.getCompletionId() + " | Command: \"" + requestCommandCompletionsC2SPacket.getPartialCommand() + "\"");
+        } else if (packet instanceof final RequestCommandCompletionsC2SPacket c2SPacket) {
+            if (this.requestCommandCompletionsC2SPacket.getValue()) {
+                ChatUtils.infoChatMessage("Outgoing request command completions packet > Id: " + c2SPacket.getCompletionId() + " | Command: \"" + c2SPacket.getPartialCommand() + "\"");
             }
         }
     }
 
     @Override
     public void onRead(final PacketEvent event) {
-        if (!this.serverToClient.getValue()) return;
-        if (event.packet instanceof final CustomPayloadS2CPacket customPayloadS2CPacket) {
+        final Packet<?> packet = event.packet;
+        if (packet instanceof final CustomPayloadS2CPacket s2CPacket) {
             if (this.customPayloadPacket.getValue()) {
-                final Identifier channel = customPayloadS2CPacket.getChannel();
+                final Identifier channel = s2CPacket.getChannel();
                 final String channelName = channel.getNamespace(), channelPath = channel.getPath();
                 ChatUtils.infoChatMessage("Incoming custom payload packet > Channel Name: " + channelName + " | Channel Path: " + channelPath);
+            }
+        } else if (packet instanceof final OpenScreenS2CPacket s2cPacket) {
+            if (this.openScreenS2CPacket.getValue()) {
+                ChatUtils.infoChatMessage("Incoming open screen packet > Name: " + s2cPacket.getName() + " | Sync Id: " + s2cPacket.getSyncId() + " | Screen Handler Type Id: " + ScreenHandlerTypes.getId(s2cPacket.getScreenHandlerType()));
             }
         }
     }
---
+++ src/main/java/me/nekosarekawaii/foxglove/injection/mixins/MixinScreenHandlerType.java
@@ -0,0 +1,26 @@
+package me.nekosarekawaii.foxglove.injection.mixins;
+
+import me.nekosarekawaii.foxglove.util.minecraft.inventory.ScreenHandlerTypes;
+import net.minecraft.resource.featuretoggle.FeatureFlag;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ScreenHandlerType.class)
+public abstract class MixinScreenHandlerType {
+
+
+    @Inject(method = "register(Ljava/lang/String;Lnet/minecraft/screen/ScreenHandlerType$Factory;)Lnet/minecraft/screen/ScreenHandlerType;", at = @At("RETURN"))
+    private static void injectRegister(final String id, final ScreenHandlerType.Factory<ScreenHandler> factory, final CallbackInfoReturnable<ScreenHandlerType<ScreenHandler>> cir) {
+        ScreenHandlerTypes.registerType(cir.getReturnValue(), id);
+    }
+
+    @Inject(method = "register(Ljava/lang/String;Lnet/minecraft/screen/ScreenHandlerType$Factory;[Lnet/minecraft/resource/featuretoggle/FeatureFlag;)Lnet/minecraft/screen/ScreenHandlerType;", at = @At("RETURN"))
+    private static void injectRegister(final String id, final ScreenHandlerType.Factory<ScreenHandler> factory, final FeatureFlag[] requiredFeatures, final CallbackInfoReturnable<ScreenHandlerType<ScreenHandler>> cir) {
+        ScreenHandlerTypes.registerType(cir.getReturnValue(), id);
+    }
+
+}
