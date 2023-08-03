cd1dc7351d69b4f0d9de8925320c1457ae1a31d1
FooFieOwO <51881617+FooFieOwO@users.noreply.github.com>
2023-08-03 12:12:14 +0200

    added lodestone tooltip (#27)

--- src/main/java/me/nekosarekawaii/foxglove/injection/mixins/MixinItemStack.java
+++ src/main/java/me/nekosarekawaii/foxglove/injection/mixins/MixinItemStack.java
@@ -5,10 +5,17 @@ import me.nekosarekawaii.foxglove.Foxglove;
 import me.nekosarekawaii.foxglove.event.TooltipListener;
 import me.nekosarekawaii.foxglove.util.minecraft.inventory.tooltip.CompoundTooltipComponent;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.client.item.TooltipData;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.CompassItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.GlobalPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -26,6 +33,40 @@ public abstract class MixinItemStack {
     private boolean injectGetDisplayStacks(final Item instance, final ItemStack stack) {
         final NbtCompound nbt = stack.getNbt();
         return instance.hasGlint(stack) || (nbt != null && nbt.contains(Foxglove.getInstance().getCreativeTabRegistry().getClientsideGlint()));
+    }
+
+    @Inject(method = "getTooltip", at = @At(value = "RETURN"))
+    private void onGetTooltip(final PlayerEntity player, final TooltipContext context, final CallbackInfoReturnable<List<Text>> cir) {
+        final List<Text> tooltip = cir.getReturnValue();
+        final ItemStack itemStack = (ItemStack) (Object) this;
+
+        if (itemStack.getItem() instanceof CompassItem && CompassItem.hasLodestone(itemStack)) {
+            var nbt = itemStack.getNbt();
+
+            if (nbt == null)
+                return;
+
+            final GlobalPos globalPos = CompassItem.createLodestonePos(nbt);
+
+            if (globalPos == null)
+                return;
+
+            final BlockPos pos = globalPos.getPos();
+            var posText = Text.literal(String.format("X: %d, Y: %d, Z: %d", pos.getX(), pos.getY(), pos.getZ()))
+                    .formatted(Formatting.GOLD);
+
+            final Text position = Text.literal("Position: ").formatted(Formatting.GRAY).append(posText);
+            final Text dimension = Text.literal("Dimension: ").formatted(Formatting.GRAY)
+                    .append(Text.literal(globalPos.getDimension().getValue().toString()).formatted(Formatting.GOLD));
+
+            if (context.isAdvanced()) {
+                tooltip.add(tooltip.size() - 2, position);
+                tooltip.add(tooltip.size() - 2, dimension);
+            } else {
+                tooltip.add(position);
+                tooltip.add(dimension);
+            }
+        }
     }
 
     @Inject(method = "getTooltipData", at = @At("RETURN"), cancellable = true)
