8f98ee2424194e5c3d86f684f76c6447da30ea58
FooFieOwO <51881617+FooFieOwO@users.noreply.github.com>
2023-08-17 16:44:15 +0200

    Merge pull request #57
    * added MoveUtil
    
    * rename

--- src/main/java/de/nekosarekawaii/foxglove/feature/impl/command/impl/movement/HClipCommand.java
+++ src/main/java/de/nekosarekawaii/foxglove/feature/impl/command/impl/movement/HClipCommand.java
@@ -2,11 +2,10 @@ package de.nekosarekawaii.foxglove.feature.impl.command.impl.movement;
 
 import com.mojang.brigadier.arguments.DoubleArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import de.nekosarekawaii.foxglove.feature.Feature;
 import de.nekosarekawaii.foxglove.feature.FeatureCategory;
 import de.nekosarekawaii.foxglove.feature.impl.command.Command;
 import de.nekosarekawaii.foxglove.feature.impl.command.CommandInfo;
-import net.minecraft.client.network.ClientPlayerEntity;
+import de.nekosarekawaii.foxglove.util.minecraft.player.MovementUtil;
 import net.minecraft.command.CommandSource;
 
 @CommandInfo(name = "H-Clip", aliases = {"hclip"}, description = "This command allows the player to clip to relative x / z positions.", category = FeatureCategory.MOVEMENT)
@@ -16,15 +15,12 @@ public class HClipCommand extends Command {
     public void build(final LiteralArgumentBuilder<CommandSource> builder) {
         builder.then(argument("horizontal", DoubleArgumentType.doubleArg(-10.0, 10.0))
                 .executes(context -> {
-                    final ClientPlayerEntity player = Feature.mc.player;
+                            final var player = mc.player;
+
                             if (player != null) {
-                                final double yaw = Math.toRadians(player.headYaw), horizontal = context.getArgument("horizontal", Double.class);
-                                player.setPos(
-                                        player.getX() - Math.sin(yaw) * horizontal,
-                                        player.getY(),
-                                        player.getZ() + Math.cos(yaw) * horizontal
-                                );
+                                MovementUtil.clip(0.0, context.getArgument("horizontal", Double.class));
                             }
+
                             return singleSuccess;
                         }
                 )
