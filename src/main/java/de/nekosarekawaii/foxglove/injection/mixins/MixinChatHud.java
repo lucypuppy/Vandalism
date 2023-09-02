5c69d20aff8f934434f5d6f24116b54e7b530b01
Lilly <51881617+FooFieOwO@users.noreply.github.com>
2023-09-02 18:29:23 +0200

    updated test showing (#76)

--- src/main/java/de/nekosarekawaii/foxglove/injection/mixins/MixinChatHud.java
+++ src/main/java/de/nekosarekawaii/foxglove/injection/mixins/MixinChatHud.java
@@ -3,22 +3,37 @@ package de.nekosarekawaii.foxglove.injection.mixins;
 import de.nekosarekawaii.foxglove.Foxglove;
 import de.nekosarekawaii.foxglove.feature.impl.module.impl.misc.MessageEncryptModule;
 import net.minecraft.client.gui.hud.ChatHud;
-import net.minecraft.text.StringVisitable;
-import net.minecraft.text.Text;
+import net.minecraft.text.*;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ChatHud.class)
 public abstract class MixinChatHud {
 
+    @Unique
+    private final MutableText prefix = Text.empty()
+            .setStyle(Style.EMPTY.withFormatting(Formatting.GRAY))
+            .append(" [")
+            .append(Text.literal("E").setStyle(Style.EMPTY.withColor(TextColor.fromRgb(0xff2626))))
+            .append("]");
+
     @ModifyArg(method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;ILnet/minecraft/client/gui/hud/MessageIndicator;Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/ChatMessages;breakRenderedChatMessageLines(Lnet/minecraft/text/StringVisitable;ILnet/minecraft/client/font/TextRenderer;)Ljava/util/List;"))
     public StringVisitable modifyMessage(final StringVisitable content) {
         final MessageEncryptModule messageEncryptModule = Foxglove.getInstance().getModuleRegistry().getMessageEncryptModule();
-        final String text = content.getString();
 
-        if (messageEncryptModule.isEnabled() && messageEncryptModule.isEncrypted(text))
-            return Text.of(messageEncryptModule.decodeMessage(text));
+        if (messageEncryptModule.isEnabled()) {
+            final MutableText text = (MutableText) content;
+            final String stringTest = text.getString();
+
+            if (messageEncryptModule.isEncrypted(stringTest)) {
+                return text.append(prefix.setStyle(prefix.getStyle().withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+                        Text.literal(messageEncryptModule.decodeMessage(stringTest))
+                ))));
+            }
+        }
 
         return content;
     }
