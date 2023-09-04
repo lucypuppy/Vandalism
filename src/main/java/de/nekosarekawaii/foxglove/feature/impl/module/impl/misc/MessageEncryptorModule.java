efdd31172bad93a651762a2b3043238bd1df82bc
Lilly <51881617+FooFieOwO@users.noreply.github.com>
2023-09-04 12:55:10 +0200

    -Updated corona module (#78)
    -Fixed bug in LiquidDensityModule

--- src/main/java/de/nekosarekawaii/foxglove/feature/impl/module/impl/misc/MessageEncryptorModule.java
+++ src/main/java/de/nekosarekawaii/foxglove/feature/impl/module/impl/misc/MessageEncryptorModule.java
@@ -5,8 +5,6 @@ import de.nekosarekawaii.foxglove.feature.impl.module.Module;
 
 public class MessageEncryptorModule extends Module {
 
-    private final static char OFFSET_CHAR = '\u3400', CHECK_CHAR = '\u3800';
-
     public MessageEncryptorModule() {
         super(
                 "Message Encryptor",
@@ -15,36 +13,6 @@ public class MessageEncryptorModule extends Module {
                 false,
                 false
         );
-    }
-
-    public String encryptMessage(final String message) {
-        final StringBuilder stringBuilder = new StringBuilder();
-        for (final char c : message.toCharArray()) {
-            stringBuilder.append((char) ((c + OFFSET_CHAR) ^ 98));
-        }
-        return stringBuilder.toString();
-    }
-
-    public String decodeMessage(final String message) {
-        final StringBuilder stringBuilder = new StringBuilder();
-        for (final char c : message.toCharArray()) {
-            if (c >= OFFSET_CHAR && c < CHECK_CHAR) {
-                stringBuilder.append((char) ((c - OFFSET_CHAR) ^ 98));
-                continue;
-            }
-
-            stringBuilder.append(c);
-        }
-        return stringBuilder.toString();
-    }
-
-    public boolean isEncrypted(final String text) {
-        for (final char c : text.toCharArray()) {
-            if (c >= OFFSET_CHAR && c < CHECK_CHAR) {
-                return true;
-            }
-        }
-        return false;
     }
 
 }
--- src/main/java/de/nekosarekawaii/foxglove/feature/impl/module/impl/movement/LiquidDensityModule.java
+++ src/main/java/de/nekosarekawaii/foxglove/feature/impl/module/impl/movement/LiquidDensityModule.java
@@ -5,6 +5,7 @@ import de.nekosarekawaii.foxglove.event.BlockListener;
 import de.nekosarekawaii.foxglove.feature.FeatureCategory;
 import de.nekosarekawaii.foxglove.feature.impl.module.Module;
 import net.minecraft.block.Block;
+import net.minecraft.block.Fertilizable;
 import net.minecraft.block.FluidBlock;
 import net.minecraft.block.FluidFillable;
 import net.minecraft.util.shape.VoxelShapes;
@@ -35,13 +36,8 @@ public class LiquidDensityModule extends Module implements BlockListener {
     public void onCollisionShapeGet(final BlockEvent event) {
         if (player() == null) return;
         final Block block = event.state.getBlock();
-        if (
-                event.pos.getY() < player().getY() &&
-                        (
-                                block instanceof FluidBlock ||
-                                        block instanceof FluidFillable
-                        )
-        ) {
+        if (event.pos.getY() < player().getY() && (block instanceof FluidBlock ||
+                (block instanceof FluidFillable && block instanceof Fertilizable))) {
             event.shape = VoxelShapes.fullCube();
             event.shouldUpdate = true;
         }
