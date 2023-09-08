d02aa39223c0afb3642e9e70657e203c92214ff2
NekosAreKawaii <nekos.are.really.cute@gmail.com>
2023-09-08 07:04:27 +0200

    Little code fix.

--- src/main/java/de/nekosarekawaii/foxglove/injection/mixins/MixinClientPlayer.java
+++ src/main/java/de/nekosarekawaii/foxglove/injection/mixins/MixinClientPlayer.java
@@ -2,7 +2,6 @@ package de.nekosarekawaii.foxglove.injection.mixins;
 
 import de.nekosarekawaii.foxglove.Foxglove;
 import de.nekosarekawaii.foxglove.util.rotation.rotationtypes.Rotation;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,23 +12,21 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public abstract class MixinClientPlayer {
 
     @Redirect(method = {"sendMovementPackets", "tick"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;getYaw()F"))
-    private float hookSilentRotationYaw(ClientPlayerEntity instance) {
+    private float redirectSendMovementPacketsAndTickYaw(final ClientPlayerEntity instance) {
         final Rotation rotation = Foxglove.getInstance().getRotationListener().getRotation();
         if (rotation != null) return rotation.getYaw();
-
         return instance.getYaw();
     }
 
     @Redirect(method = {"sendMovementPackets", "tick"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;getPitch()F"))
-    private float hookSilentRotationPitch(ClientPlayerEntity instance) {
+    private float redirectSendMovementPacketsAndTickPitch(final ClientPlayerEntity instance) {
         final Rotation rotation = Foxglove.getInstance().getRotationListener().getRotation();
         if (rotation != null) return rotation.getPitch();
-
         return instance.getPitch();
     }
 
     @Redirect(method = "updateNausea", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;shouldPause()Z"))
-    public boolean updateNausea(Screen screen) {
+    public boolean redirectUpdateNausea(final Screen screen) {
         return Foxglove.getInstance().getConfigManager().getMainConfig().portalScreen.getValue() || screen.shouldPause();
     }
 
