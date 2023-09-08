bdffda41907bccb2f8a2594e558c2cb449442eef
NekosAreKawaii <nekos.are.really.cute@gmail.com>
2023-09-08 06:52:04 +0200

    Fixed riptide boost multiplier code.

--- src/main/java/de/nekosarekawaii/foxglove/feature/impl/module/impl/movement/LiquidDensityModule.java
+++ src/main/java/de/nekosarekawaii/foxglove/feature/impl/module/impl/movement/LiquidDensityModule.java
@@ -1,13 +1,15 @@
 package de.nekosarekawaii.foxglove.feature.impl.module.impl.movement;
 
 import de.florianmichael.dietrichevents2.DietrichEvents2;
-import de.nekosarekawaii.foxglove.Foxglove;
 import de.nekosarekawaii.foxglove.event.BlockListener;
 import de.nekosarekawaii.foxglove.feature.FeatureCategory;
 import de.nekosarekawaii.foxglove.feature.impl.module.Module;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FluidBlock;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.shape.VoxelShapes;
 
 public class LiquidDensityModule extends Module implements BlockListener {
@@ -34,7 +36,14 @@ public class LiquidDensityModule extends Module implements BlockListener {
 
     @Override
     public void onCollisionShapeGet(final BlockEvent event) {
-        if (player() == null || Foxglove.getInstance().getModuleRegistry().getRiptideBoostMultiplierModule().isExempted()) return;
+        if (player() == null) return;
+        if (options().useKey.isPressed()) {
+            final ItemStack mainHandStack = player().getMainHandStack();
+            if (mainHandStack.getItem() == Items.TRIDENT && EnchantmentHelper.getRiptide(mainHandStack) > 0) {
+                return;
+            }
+        }
+        if (player().isUsingRiptide()) return;
         final BlockState state = event.state;
         final Block block = state.getBlock();
         if (event.pos.getY() < player().getY() && (block instanceof FluidBlock || !block.getFluidState(state).isEmpty())) {
--- src/main/java/de/nekosarekawaii/foxglove/injection/mixins/MixinTridentItem.java
+++ src/main/java/de/nekosarekawaii/foxglove/injection/mixins/MixinTridentItem.java
@@ -10,19 +10,12 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 @Mixin(TridentItem.class)
 public abstract class MixinTridentItem {
 
-    /**
-     * I need to cast to double because Mixin will cry otherwise :(
-     * 0, 1, 2 are the x, y, z values which are all doubles.
-     *
-     * @param args arguments of addVelocity that we want to change
-     * @author Lucy
-     */
     @ModifyArgs(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;addVelocity(DDD)V"))
-    public void modifyOnStoppedUsing(final Args args) {
-        if (!Foxglove.getInstance().getModuleRegistry().getRiptideBoostMultiplierModule().isEnabled()) return;
-
-        args.set(0, (double) args.get(0) * Foxglove.getInstance().getModuleRegistry().getRiptideBoostMultiplierModule().multiplier.getValue());
-        args.set(1, (double) args.get(1) * Foxglove.getInstance().getModuleRegistry().getRiptideBoostMultiplierModule().multiplier.getValue());
-        args.set(2, (double) args.get(2) * Foxglove.getInstance().getModuleRegistry().getRiptideBoostMultiplierModule().multiplier.getValue());
+    public void modifyArgsOnStoppedUsing(final Args args) {
+        if (!Foxglove.getInstance().getConfigManager().getMainConfig().customizeRiptideBoostMultiplier.getValue())
+            return;
+        final float multiplier = Foxglove.getInstance().getConfigManager().getMainConfig().riptideBoostMultiplier.getValue();
+        for (int i = 0; i < 2; i++) args.set(i, (double) args.get(i) * multiplier);
     }
+
 }
