307dfd0482c68dd1f9a38b095e93d54e5398c511
NekosAreKawaii <nekos.are.really.cute@gmail.com>
2023-09-10 11:24:14 +0200

    Fixed code from the ListValue.

--- src/main/java/de/nekosarekawaii/foxglove/value/values/ListValue.java
+++ src/main/java/de/nekosarekawaii/foxglove/value/values/ListValue.java
@@ -1,40 +1,24 @@
 package de.nekosarekawaii.foxglove.value.values;
 
-import com.google.gson.JsonObject;
 import de.nekosarekawaii.foxglove.value.IValue;
-import de.nekosarekawaii.foxglove.value.Value;
 import imgui.ImGui;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class ListValue extends Value<String> {
+public class ListValue extends StringValue {
 
-    private final List<String> modes;
+    private final List<String> values;
 
-    public ListValue(final String name, final String description, final IValue parent, final String... modes) {
-        super(name, description, parent, modes[0]);
-        this.modes = Arrays.asList(modes);
-    }
-
-    public List<String> getModes() {
-        return this.modes;
-    }
-
-    @Override
-    public void onConfigLoad(final JsonObject valueObject) {
-        this.setValue(valueObject.get("value").getAsString());
-    }
-
-    @Override
-    public void onConfigSave(final JsonObject valueObject) {
-        valueObject.addProperty("value", getValue());
+    public ListValue(final String name, final String description, final IValue parent, final String... values) {
+        super(name, description, parent, values[0]);
+        this.values = Arrays.asList(values);
     }
 
     @Override
     public void render() {
         if (ImGui.beginCombo(this.getName() + "##" + this.getHashIdent(), getValue())) {
-            for (final String mode : getModes()) {
+            for (final String mode : this.values) {
                 if (ImGui.selectable(mode, mode.equals(getValue()))) {
                     this.setValue(mode);
                 }
