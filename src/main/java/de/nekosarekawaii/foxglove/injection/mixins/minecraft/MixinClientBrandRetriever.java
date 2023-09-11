e4c7932bf849e11d77f5c2e79b6f9769b795bb51
NekosAreKawaii <nekos.are.really.cute@gmail.com>
2023-09-11 16:09:26 +0200

    Fixed Client Brand Changer Module.

--- src/main/java/de/nekosarekawaii/foxglove/injection/mixins/minecraft/MixinClientBrandRetriever.java
+++ src/main/java/de/nekosarekawaii/foxglove/injection/mixins/minecraft/MixinClientBrandRetriever.java
@@ -3,9 +3,7 @@ package de.nekosarekawaii.foxglove.injection.mixins.minecraft;
 import de.nekosarekawaii.foxglove.Foxglove;
 import de.nekosarekawaii.foxglove.feature.impl.module.impl.exploit.ClientBrandChangerModule;
 import net.minecraft.client.ClientBrandRetriever;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -13,19 +11,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(value = ClientBrandRetriever.class, priority = 9999)
 public abstract class MixinClientBrandRetriever {
 
-    @Shadow
-    @Final
-    public static String VANILLA;
-
-    @Inject(method = "getClientModName", at = @At("RETURN"), cancellable = true, remap = false)
-    private static void injectGetClientModName(CallbackInfoReturnable<String> cir) {
+    @Inject(method = "getClientModName", at = @At("HEAD"), cancellable = true, remap = false)
+    private static void injectGetClientModName(final CallbackInfoReturnable<String> cir) {
         if (Foxglove.getInstance() != null && Foxglove.getInstance().getModuleRegistry() != null) {
             final ClientBrandChangerModule clientBrandChangerModule = Foxglove.getInstance().getModuleRegistry().getClientBrandChangerModule();
-            if (clientBrandChangerModule.isEnabled()) {
-                cir.setReturnValue(clientBrandChangerModule.brand.getValue());
-            }
+            if (clientBrandChangerModule.isEnabled()) cir.setReturnValue(clientBrandChangerModule.brand.getValue());
         }
-        cir.setReturnValue(VANILLA);
     }
 
 }
