8549c643edd0683c55cabd9ccd1da0ea91912fdb
Lucy Adler <117228413+cuddlycatgirl@users.noreply.github.com>
2023-09-12 05:17:45 +0200

    Merge pull request #87
    * Cleaned up HUD & added extra value category for info elements so it i…

--- src/main/java/de/nekosarekawaii/foxglove/feature/impl/module/impl/render/HeadUpDisplayModule.java
+++ src/main/java/de/nekosarekawaii/foxglove/feature/impl/module/impl/render/HeadUpDisplayModule.java
@@ -8,6 +8,7 @@ import de.nekosarekawaii.foxglove.feature.FeatureList;
 import de.nekosarekawaii.foxglove.feature.impl.module.Module;
 import de.nekosarekawaii.foxglove.gui.imgui.ImGuiUtil;
 import de.nekosarekawaii.foxglove.value.Value;
+import de.nekosarekawaii.foxglove.value.ValueCategory;
 import de.nekosarekawaii.foxglove.value.values.BooleanValue;
 import imgui.ImGui;
 import imgui.flag.ImGuiWindowFlags;
@@ -34,6 +35,13 @@ public class HeadUpDisplayModule extends Module implements RenderListener {
             true
     );
 
+    private final Value<Boolean> watermark = new BooleanValue(
+            "Watermark",
+            "Shows the watermark.",
+            this,
+            true
+    );
+
     private final Value<Boolean> infos = new BooleanValue(
             "Infos",
             "Shows general infos.",
@@ -41,45 +49,51 @@ public class HeadUpDisplayModule extends Module implements RenderListener {
             true
     );
 
+    private final ValueCategory infoElements = new ValueCategory(
+            "Info Elements",
+            "Elements that are shown in the infos category.",
+            this
+    ).visibleConsumer(this.infos::getValue);
+
     private final Value<Boolean> fps = new BooleanValue(
             "FPS",
             "Shows the current fps.",
-            this,
+            infoElements,
             true
     ).visibleConsumer(this.infos::getValue);
 
     private final Value<Boolean> username = new BooleanValue(
             "Username",
             "Shows the current username.",
-            this,
+            infoElements,
             true
     ).visibleConsumer(this.infos::getValue);
 
     private final Value<Boolean> position = new BooleanValue(
             "Position",
             "Shows the current position.",
-            this,
+            infoElements,
             true
     ).visibleConsumer(this.infos::getValue);
 
     private final Value<Boolean> serverBrand = new BooleanValue(
             "Server Brand",
             "Shows the current server brand.",
-            this,
+            infoElements,
             true
     ).visibleConsumer(this.infos::getValue);
 
     private final Value<Boolean> difficulty = new BooleanValue(
             "Difficulty",
             "Shows the current world difficulty.",
-            this,
+            infoElements,
             true
     ).visibleConsumer(this.infos::getValue);
 
     private final Value<Boolean> permissionsLevel = new BooleanValue(
             "Permissions Level",
             "Shows the current permissions level.",
-            this,
+            infoElements,
             true
     ).visibleConsumer(this.infos::getValue);
 
@@ -126,60 +140,76 @@ public class HeadUpDisplayModule extends Module implements RenderListener {
 
     private void render() {
         Foxglove.getInstance().getImGuiHandler().getImGuiRenderer().addRenderInterface(io -> {
+
             if (this.sort) {
                 this.sort = false;
                 this.enabledModules.clear();
                 final FeatureList<Module> modules = Foxglove.getInstance().getModuleRegistry().getModules();
+
                 for (final Module module : modules) {
                     final boolean display = module.isEnabled() && module.isShowInModuleList();
                     final String toDisplay = module.getName();
                     if (display) this.enabledModules.add(toDisplay);
                 }
+
                 this.enabledModules.sort((s1, s2) -> Float.compare(ImGui.calcTextSize(s2).x, ImGui.calcTextSize(s1).x));
             }
+
             if (player() == null || world() == null || options().debugEnabled || options().hudHidden || currentScreen() instanceof InventoryScreen)
                 return;
+
             final int windowFlags = this.transparent.getValue() ? ImGuiUtil.getInGameFlags(0) : (mouse().isCursorLocked() ? ImGuiWindowFlags.NoCollapse : 0);
-            if (ImGui.begin("Watermark##headupdisplaymodule", windowFlags | ImGuiWindowFlags.NoResize)) {
-                ImGui.setWindowSize(0, 0);
-                ImGui.text(Foxglove.getInstance().getName() + "\tv" + Foxglove.getInstance().getVersion());
-                ImGui.text("Made by " + Foxglove.getInstance().getAuthorsAsString());
-                ImGui.end();
+
+            // Render Watermark
+            if (watermark.getValue()) {
+                if (ImGui.begin("Watermark##headupdisplaymodule", windowFlags | ImGuiWindowFlags.NoResize)) {
+                    ImGui.setWindowSize(0, 0);
+                    ImGui.text(Foxglove.getInstance().getName() + "\tv" + Foxglove.getInstance().getVersion());
+                    ImGui.text("Made by " + Foxglove.getInstance().getAuthorsAsString());
+                    ImGui.end();
+                }
             }
+
+            // Render module list
             if (this.enabledModulesList.getValue()) {
                 if (ImGui.begin("Enabled Modules##headupdisplaymodule", windowFlags)) {
                     final boolean empty = this.enabledModules.isEmpty();
+
                     for (final String enabledModule : this.enabledModules) {
                         ImGui.text(enabledModule);
                     }
+
                     ImGui.setWindowSize(empty ? 100 : 0, empty ? 50 : 0);
                     ImGui.end();
                 }
             }
+
+            // Render infos
             if (this.infos.getValue()) {
                 if (ImGui.begin("Infos##headupdisplaymodule", windowFlags)) {
                     ImGui.setWindowSize(0, 0);
-                    if (this.fps.getValue()) {
+
+                    if (this.fps.getValue())
                         ImGui.text("FPS: " + mc().getCurrentFps());
-                    }
-                    if (this.username.getValue()) {
+
+                    if (this.username.getValue())
                         ImGui.text("Username: " + player().getGameProfile().getName());
-                    }
-                    if (this.position.getValue()) {
+
+                    if (this.position.getValue())
                         ImGui.text("Position: " + player().getBlockPos().toShortString());
-                    }
+
                     if (this.serverBrand.getValue()) {
                         final String serverBrand = player().getServerBrand();
-                        if (serverBrand != null) {
+                        if (serverBrand != null)
                             ImGui.text("Server Brand: " + serverBrand.replaceFirst("\\(.*?\\) ", ""));
-                        }
                     }
-                    if (this.difficulty.getValue()) {
+
+                    if (this.difficulty.getValue())
                         ImGui.text("Difficulty: " + world().getDifficulty().getName());
-                    }
-                    if (this.permissionsLevel.getValue()) {
+
+                    if (this.permissionsLevel.getValue())
                         ImGui.text("Permissions Level: " + player().getPermissionLevel());
-                    }
+
                     ImGui.end();
                 }
             }
