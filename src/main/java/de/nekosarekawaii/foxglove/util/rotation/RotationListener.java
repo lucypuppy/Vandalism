dec8fd14fea674b06fab902f830c8c79c80eda14
Lilly <51881617+FooFieOwO@users.noreply.github.com>
2023-09-13 16:05:42 +0200

    Small update (#89)
    -Moved rotation build methods into Rotation.Builder
    -Fixed a small thing in GCD Fix

--- src/main/java/de/nekosarekawaii/foxglove/util/rotation/RotationListener.java
+++ src/main/java/de/nekosarekawaii/foxglove/util/rotation/RotationListener.java
@@ -4,19 +4,14 @@ import de.florianmichael.dietrichevents2.DietrichEvents2;
 import de.nekosarekawaii.foxglove.event.PacketListener;
 import de.nekosarekawaii.foxglove.event.RenderListener;
 import de.nekosarekawaii.foxglove.util.MinecraftWrapper;
-import de.nekosarekawaii.foxglove.util.RaytraceUtil;
 import de.nekosarekawaii.foxglove.util.render.RenderUtils;
 import de.nekosarekawaii.foxglove.util.rotation.rotationtypes.Rotation;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.Window;
-import net.minecraft.entity.Entity;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec2f;
-import net.minecraft.util.math.Vec3d;
 import org.apache.commons.lang3.RandomUtils;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class RotationListener implements PacketListener, RenderListener, MinecraftWrapper {
@@ -83,83 +78,12 @@ public class RotationListener implements PacketListener, RenderListener, Minecra
         }
     }
 
-    public void setRotation(final @NotNull Vec3d to, final Vec2f rotateSpeedMinMax, final RotationPriority priority) {
-        if (player() == null) return;
-
-        this.setRotation(vecToRotation(to, player().getEyePos()), rotateSpeedMinMax, priority);
-    }
-
-    public void setRotation(final Entity entity, final boolean bestHitVec, final double range, final Vec2f rotateSpeedMinMax, final RotationPriority priority) {
-        if (player() == null) return;
-
-        final Vec3d eyePos = player().getEyePos();
-        final Box box = entity.getBoundingBox();
-        final Vec3d getEntityVector = bestHitVec ? getNearestPoint(entity) : new Vec3d(entity.getX(), entity.getY(), entity.getZ());
-
-        Rotation normalRotations = vecToRotation(getEntityVector, eyePos);
-        if (RaytraceUtil.rayTraceBlock(normalRotations.getVector(), range))
-            setRotation(normalRotations, rotateSpeedMinMax, priority);
-
-        Vec3d currentVector = null;
-        for (double x = 0.00D; x < 1.00D; x += 0.1D) {
-            for (double y = 0.00D; y < 1.00D; y += 0.1D) {
-                for (double z = 0.00D; z < 1.00D; z += 0.1D) {
-                    final Vec3d vector = new Vec3d(
-                            box.minX + (box.maxX - box.minX) * x,
-                            box.minY + (box.maxY - box.minY) * y,
-                            box.minZ + (box.maxZ - box.minZ) * z);
-
-                    if (eyePos.distanceTo(vector) > range)
-                        continue;
-
-                    final Rotation parsedRotation = vecToRotation(vector, eyePos);
-
-                    if (!RaytraceUtil.rayTraceBlock(parsedRotation.getVector(), range))
-                        continue;
-
-                    if (!bestHitVec) {
-                        setRotation(parsedRotation, rotateSpeedMinMax, priority);
-                    } else if (currentVector == null || eyePos.distanceTo(vector) <= eyePos.distanceTo(currentVector)) {
-                        currentVector = vector;
-                        normalRotations = parsedRotation;
-                    }
-                }
-            }
-        }
-
-        setRotation(normalRotations, rotateSpeedMinMax, priority);
-    }
-
-    public Rotation vecToRotation(final Vec3d to, final Vec3d eyePos) {
-        final Vec3d diff = to.subtract(eyePos);
-        final double hypot = Math.hypot(diff.getX(), diff.getZ());
-        final float
-                yaw = (float) (MathHelper.atan2(diff.getZ(), diff.getX()) * (180.0F / Math.PI)) - 90.0F,
-                pitch = (float) (-MathHelper.atan2(diff.getY(), hypot) * (180.0F / Math.PI));
-
-        return new Rotation(yaw, pitch);
-    }
-
-    public Vec3d getNearestPoint(final Entity entity) {
-        final Box boundingBox = entity.getBoundingBox();
-
-        final double nearestX = Math.max(boundingBox.minX, Math.min(entity.getX(), boundingBox.maxX));
-        final double nearestZ = Math.max(boundingBox.minZ, Math.min(entity.getZ(), boundingBox.maxZ));
-
-        //Todo recode this.
-        final double nearestY = entity.getY() + Math.max(0,
-                Math.min(player().getY() - entity.getY() + player().getEyeHeight(player().getPose()),
-                        (boundingBox.maxY - boundingBox.minY) * 0.9)
-        );
-
-        return new Vec3d(nearestX, nearestY, nearestZ);
-    }
-
     // Bruteforce GCD Method best for hvh tested it really often.
     private Rotation bruteforceGCD(final Rotation rotation, final float partialTicks) {
         float yaw = rotation.getYaw(), pitch = rotation.getPitch();
 
-        final double multiplier = getMouseMultiplier();
+        final double sensitivity = mc().options.getMouseSensitivity().getValue();
+        final double multiplier = getMouseMultiplier(sensitivity) * 0.15D;
 
         double iterationsNeeded = RenderUtils.getFps() / 20.0;
         iterationsNeeded *= partialTicks;
@@ -179,9 +103,14 @@ public class RotationListener implements PacketListener, RenderListener, Minecra
         return new Rotation(wrapDegreesFixed(yaw), pitch);
     }
 
-    private double getMouseMultiplier() {
-        final double f = options().getMouseSensitivity().getValue() * 0.6F + 0.2F;
-        return f * f * f * 8.0;
+    private double getMouseMultiplier(final double sensitivity) {
+        final double f = sensitivity * 0.6F + 0.2F;
+        final double g = f * f * f;
+
+        if (mc().options.getPerspective().isFirstPerson() && mc().player.isUsingSpyglass())
+            return g;
+
+        return g * 8.0;
     }
 
     // This is a fix method for rounding errors in the yaw detectable with a simple check.
