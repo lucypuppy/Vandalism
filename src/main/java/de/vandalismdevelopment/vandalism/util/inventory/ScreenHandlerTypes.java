b4a25452015249f9c3425c0738d2adf367e907a8
Adrian Merschel <137268600+NekosAreKawaii@users.noreply.github.com>
2023-10-12 12:26:57 +0200

    Made the hashmap variable name uppercase. (#100)
    * Made the hashmap variable name uppercase.
    
    * Changed order of static and final to solve an error from the workflow.

--- src/main/java/de/vandalismdevelopment/vandalism/util/inventory/ScreenHandlerTypes.java
+++ src/main/java/de/vandalismdevelopment/vandalism/util/inventory/ScreenHandlerTypes.java
@@ -6,20 +6,20 @@ import java.util.HashMap;
 
 public class ScreenHandlerTypes {
 
-    private final static HashMap<Integer, String> screenHandlerTypeIdMap = new HashMap<>();
+    private static final HashMap<Integer, String> SCREEN_HANDLER_TYPE_ID_MAP = new HashMap<>();
 
     public static String getId(final ScreenHandlerType<?> screenHandlerType) {
         if (screenHandlerType != null) {
             final int hashCode = screenHandlerType.hashCode();
-            if (screenHandlerTypeIdMap.containsKey(hashCode)) {
-                return screenHandlerTypeIdMap.get(hashCode);
+            if (SCREEN_HANDLER_TYPE_ID_MAP.containsKey(hashCode)) {
+                return SCREEN_HANDLER_TYPE_ID_MAP.get(hashCode);
             }
         }
         return "";
     }
 
     public static void registerType(final ScreenHandlerType<?> screenHandlerType, final String id) {
-        screenHandlerTypeIdMap.put(screenHandlerType.hashCode(), id);
+        SCREEN_HANDLER_TYPE_ID_MAP.put(screenHandlerType.hashCode(), id);
     }
 
 }
