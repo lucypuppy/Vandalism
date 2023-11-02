b5751ea36215eda427ffe26f39437c6dd0b64b53
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2023-11-02 21:52:22 +0100

    Improved chat clear command.

--- src/main/java/de/vandalismdevelopment/vandalism/feature/impl/command/impl/misc/ChatClearCommand.java
+++ src/main/java/de/vandalismdevelopment/vandalism/feature/impl/command/impl/misc/ChatClearCommand.java
@@ -1,16 +1,21 @@
 package de.vandalismdevelopment.vandalism.feature.impl.command.impl.misc;
 
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import de.vandalismdevelopment.vandalism.Vandalism;
 import de.vandalismdevelopment.vandalism.feature.FeatureCategory;
 import de.vandalismdevelopment.vandalism.feature.impl.command.Command;
 import net.minecraft.command.CommandSource;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ChatClearCommand extends Command {
 
     public ChatClearCommand() {
         super(
                 "Chat Clear",
-                "Clears your clientside chat.",
+                "Clears your clientside chat (also allows you to clear your sent history).",
                 FeatureCategory.MISC,
                 false,
                 "chatclear",
@@ -25,6 +30,29 @@ public class ChatClearCommand extends Command {
             mc().inGameHud.getChatHud().clear(false);
             return SINGLE_SUCCESS;
         });
+        builder.then(argument("clear-sent-history", BoolArgumentType.bool())
+                .executes(context -> {
+                    final boolean clearSentHistory = BoolArgumentType.getBool(context, "clear-sent-history");
+                    if (clearSentHistory) {
+                        final File commandHistoryFile = new File(mc().runDirectory, "command_history.txt");
+                        if (commandHistoryFile.exists()) {
+                            if (!commandHistoryFile.delete()) {
+                                Vandalism.getInstance().getLogger().error("Failed to delete command history file.");
+                            } else {
+                                try {
+                                    if (!commandHistoryFile.createNewFile()) {
+                                        Vandalism.getInstance().getLogger().error("Failed to create new command history file.");
+                                    }
+                                } catch (final IOException e) {
+                                    Vandalism.getInstance().getLogger().error("Failed to create new command history file.", e);
+                                }
+                            }
+                        }
+                    }
+                    mc().inGameHud.getChatHud().clear(clearSentHistory);
+                    return SINGLE_SUCCESS;
+                })
+        );
     }
 
 }
