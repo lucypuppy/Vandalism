344a7806abef8546e5e32b919450dc06ed76b5ef
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2023-11-02 21:34:22 +0100

    Re-added every dropped feature.

--- src/main/java/de/vandalismdevelopment/vandalism/creativetab/impl/GriefItemsCreativeTab.java
+++ src/main/java/de/vandalismdevelopment/vandalism/creativetab/impl/GriefItemsCreativeTab.java
@@ -1,7 +1,7 @@
 package de.vandalismdevelopment.vandalism.creativetab.impl;
 
 import de.vandalismdevelopment.vandalism.creativetab.CreativeTab;
-import net.minecraft.item.Item;
+import de.vandalismdevelopment.vandalism.util.ItemUtil;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
@@ -22,57 +22,48 @@ public class GriefItemsCreativeTab extends CreativeTab {
     @Override
     public Collection<ItemStack> entries() {
         final Collection<ItemStack> current = super.entries();
-
-        current.add(createItem(createGriefCreeper(), Text.literal(Formatting.RED + Formatting.BOLD.toString() + "Grief Creeper")));
-        current.add(createItem(createPoweredGriefCreeper(), Text.literal(Formatting.GOLD + Formatting.BOLD.toString() + "Powered " + Formatting.RED + Formatting.BOLD + "Grief Creeper"), true, "NekosAreKawaii"));
-        current.add(createItem(createSpawnEgg(Items.SHEEP_SPAWN_EGG, "minecraft:ender_dragon"), Text.literal(Formatting.LIGHT_PURPLE + Formatting.BOLD.toString() + "Ender Dragon")));
-        current.add(createItem(createSpawnEgg(Items.COW_SPAWN_EGG, "minecraft:wither"), Text.literal(Formatting.WHITE + Formatting.BOLD.toString() + "Wither")));
-
+        current.add(ItemUtil.createItemStack(
+                this.createGriefCreeper(),
+                Text.literal(Formatting.RED + Formatting.BOLD.toString() + "Grief Creeper")
+        ));
+        current.add(ItemUtil.createItemStack(
+                this.createPoweredGriefCreeper(),
+                Text.literal(Formatting.GOLD + Formatting.BOLD.toString() + "Powered " + Formatting.RED + Formatting.BOLD + "Grief Creeper"),
+                true
+        ));
+        current.add(ItemUtil.createItemStack(
+                ItemUtil.createSpawnEggItemStack(Items.SHEEP_SPAWN_EGG, "minecraft:ender_dragon"),
+                Text.literal(Formatting.LIGHT_PURPLE + Formatting.BOLD.toString() + "Ender Dragon")
+        ));
+        current.add(ItemUtil.createItemStack(
+                ItemUtil.createSpawnEggItemStack(Items.COW_SPAWN_EGG, "minecraft:wither"),
+                Text.literal(Formatting.WHITE + Formatting.BOLD.toString() + "Wither")
+        ));
         return current;
     }
 
     private ItemStack createGriefCreeper() {
         final ItemStack item = new ItemStack(Items.PIG_SPAWN_EGG);
         final NbtCompound base = new NbtCompound();
-
         final NbtCompound entityTag = new NbtCompound();
         entityTag.putInt("Fuse", 0);
         entityTag.putInt("ExplosionRadius", 127);
         entityTag.putString("id", "minecraft:creeper");
-
         base.put("EntityTag", entityTag);
-
         item.setNbt(base);
-
         return item;
     }
 
     private ItemStack createPoweredGriefCreeper() {
         final ItemStack item = new ItemStack(Items.COW_SPAWN_EGG);
         final NbtCompound base = new NbtCompound();
-
         final NbtCompound entityTag = new NbtCompound();
         entityTag.putInt("Fuse", 0);
         entityTag.putInt("ExplosionRadius", 127);
         entityTag.putByte("powered", (byte) 1);
         entityTag.putString("id", "minecraft:creeper");
-
         base.put("EntityTag", entityTag);
         item.setNbt(base);
-
-        return item;
-    }
-
-    private ItemStack createSpawnEgg(final Item origin, final String id) {
-        final ItemStack item = new ItemStack(origin);
-        final NbtCompound base = new NbtCompound();
-
-        final NbtCompound entityTag = new NbtCompound();
-        entityTag.putString("id", id);
-
-        base.put("EntityTag", entityTag);
-        item.setNbt(base);
-
         return item;
     }
 
