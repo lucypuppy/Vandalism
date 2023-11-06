62d53d6c541ace2fdf6652c10222c81bcbf550e7
NekosAreKawaii <admin@nekosarekawaii.de>
2023-11-06 01:18:00 +0100

    Made it able to escape disconnect screen via. escape key.

--- src/main/java/de/vandalismdevelopment/vandalism/injection/mixins/minecraft/MixinDisconnectedScreen.java
+++ src/main/java/de/vandalismdevelopment/vandalism/injection/mixins/minecraft/MixinDisconnectedScreen.java
@@ -5,12 +5,19 @@ import net.minecraft.client.gui.screen.DisconnectedScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.*;
 import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(DisconnectedScreen.class)
 public abstract class MixinDisconnectedScreen extends Screen {
+
+    @Shadow
+    @Final
+    private Screen parent;
 
     protected MixinDisconnectedScreen(final Text ignored) {
         super(ignored);
@@ -33,11 +40,19 @@ public abstract class MixinDisconnectedScreen extends Screen {
                 }
             });
             final String text = textBuilder.toString();
-            if (client != null && !text.isBlank()) {
-                client.keyboard.setClipboard(text);
+            if (this.client != null && !text.isBlank()) {
+                this.client.keyboard.setClipboard(text);
             }
         }).build(), positioner);
         return widget;
+    }
+
+    @Override
+    public boolean keyPressed(final int keyCode, final int scanCode, final int modifiers) {
+        if (keyCode == GLFW.GLFW_KEY_ESCAPE && this.client != null) {
+            this.client.setScreen(this.parent);
+        }
+        return super.keyPressed(keyCode, scanCode, modifiers);
     }
 
 }
