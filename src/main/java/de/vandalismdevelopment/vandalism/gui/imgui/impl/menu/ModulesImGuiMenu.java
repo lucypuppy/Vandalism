b8158f6105f4e4b2da5c251589c69774971ea5d7
NekosAreKawaii <admin@nekosarekawaii.de>
2023-11-08 13:31:11 +0100

    Final fixes for the modules menu.

--- src/main/java/de/vandalismdevelopment/vandalism/gui/imgui/impl/menu/ModulesImGuiMenu.java
+++ src/main/java/de/vandalismdevelopment/vandalism/gui/imgui/impl/menu/ModulesImGuiMenu.java
@@ -34,76 +34,75 @@ public class ModulesImGuiMenu extends ImGuiMenu {
                         ImGuiWindowFlags.NoCollapse |
                                 ImGuiWindowFlags.NoResize |
                                 ImGuiWindowFlags.NoScrollbar |
-                                ImGuiWindowFlags.NoScrollWithMouse
+                                ImGuiWindowFlags.NoScrollWithMouse |
+                                ImGuiWindowFlags.NoDocking
                 )) {
                     ImGui.separator();
                     if (ImGui.beginChild(featureCategoryIdentifier + "scrolllist", -1, -1, true)) {
-                        for (int i = 0; i < 10; i++) {
-                            for (final Module module : modulesByCategory) {
-                                final float[] color;
-                                if (module.isEnabled()) {
-                                    color = new float[]{
-                                            0.1f, 0.8f, 0.1f, 0.45f
-                                    };
+                        for (final Module module : modulesByCategory) {
+                            final float[] color;
+                            if (module.isEnabled()) {
+                                color = new float[]{
+                                        0.1f, 0.8f, 0.1f, 0.45f
+                                };
+                            } else {
+                                color = new float[]{
+                                        0.8f, 0.1f, 0.1f, 0.45f
+                                };
+                            }
+                            final boolean moduleEnabled = module.isEnabled();
+                            if (moduleEnabled) {
+                                ImGui.pushStyleColor(ImGuiCol.Button, color[0], color[1], color[2], color[3]);
+                                ImGui.pushStyleColor(ImGuiCol.ButtonHovered, color[0], color[1], color[2], color[3] - 0.1f);
+                                ImGui.pushStyleColor(ImGuiCol.ButtonActive, color[0], color[1], color[2], color[3] + 0.1f);
+                            }
+                            final String moduleIdentifier = "##" + module.getName() + module.getCategory().normalName() + "module" + module.getName();
+                            if (ImGui.button(module.getName() + moduleIdentifier + "togglebutton", -1, 25)) {
+                                module.toggle();
+                            }
+                            if (moduleEnabled) {
+                                ImGui.popStyleColor(3);
+                            }
+                            if (ImGui.beginPopupContextItem(moduleIdentifier + "configmenu", ImGuiPopupFlags.MouseButtonRight)) {
+                                ImGui.text(module.getName() + " Module");
+                                ImGui.separator();
+                                ImGui.spacing();
+                                final List<Value<?>> values = module.getValues();
+                                final List<String> descriptionLines = new ArrayList<>();
+                                final String[] descriptionWords = module.getDescription().split(" ");
+                                if (descriptionWords.length > 10) {
+                                    StringBuilder currentLine = new StringBuilder();
+                                    for (final String descriptionWord : descriptionWords) {
+                                        if (currentLine.length() + descriptionWord.length() > 50) {
+                                            descriptionLines.add(currentLine.toString());
+                                            currentLine = new StringBuilder();
+                                        }
+                                        currentLine.append(descriptionWord).append(" ");
+                                    }
+                                    descriptionLines.add(currentLine.toString());
                                 } else {
-                                    color = new float[]{
-                                            0.8f, 0.1f, 0.1f, 0.45f
-                                    };
+                                    descriptionLines.add(module.getDescription());
                                 }
-                                final boolean moduleEnabled = module.isEnabled();
-                                if (moduleEnabled) {
-                                    ImGui.pushStyleColor(ImGuiCol.Button, color[0], color[1], color[2], color[3]);
-                                    ImGui.pushStyleColor(ImGuiCol.ButtonHovered, color[0], color[1], color[2], color[3] - 0.1f);
-                                    ImGui.pushStyleColor(ImGuiCol.ButtonActive, color[0], color[1], color[2], color[3] + 0.1f);
+                                for (final String descriptionLine : descriptionLines) {
+                                    ImGui.text(descriptionLine);
                                 }
-                                final String moduleIdentifier = "##" + module.getName() + module.getCategory().normalName() + "module" + module.getName();
-                                if (ImGui.button(module.getName() + moduleIdentifier + "togglebutton", -1, 25)) {
-                                    module.toggle();
+                                ImGui.spacing();
+                                if (module.isExperimental()) {
+                                    ImGui.textColored(0.8f, 0.1f, 0.1f, 1f, "Warning this is a unstable experimental module!");
                                 }
-                                if (moduleEnabled) {
-                                    ImGui.popStyleColor(3);
-                                }
-                                if (ImGui.beginPopupContextItem(moduleIdentifier + "configmenu", ImGuiPopupFlags.MouseButtonRight)) {
-                                    ImGui.text(module.getName() + " Module");
+                                ImGui.separator();
+                                if (!values.isEmpty()) {
+                                    ImGui.text("Config");
                                     ImGui.separator();
-                                    ImGui.spacing();
-                                    final List<Value<?>> values = module.getValues();
-                                    final List<String> descriptionLines = new ArrayList<>();
-                                    final String[] descriptionWords = module.getDescription().split(" ");
-                                    if (descriptionWords.length > 10) {
-                                        StringBuilder currentLine = new StringBuilder();
-                                        for (final String descriptionWord : descriptionWords) {
-                                            if (currentLine.length() + descriptionWord.length() > 50) {
-                                                descriptionLines.add(currentLine.toString());
-                                                currentLine = new StringBuilder();
-                                            }
-                                            currentLine.append(descriptionWord).append(" ");
+                                    if (ImGui.button("Reset config" + moduleIdentifier + "resetconfigbutton")) {
+                                        for (final Value<?> value : values) {
+                                            value.resetValue();
                                         }
-                                        descriptionLines.add(currentLine.toString());
-                                    } else {
-                                        descriptionLines.add(module.getDescription());
                                     }
-                                    for (final String descriptionLine : descriptionLines) {
-                                        ImGui.text(descriptionLine);
-                                    }
-                                    ImGui.spacing();
-                                    if (module.isExperimental()) {
-                                        ImGui.textColored(0.8f, 0.1f, 0.1f, 1f, "Warning this is a unstable experimental module!");
-                                    }
+                                    module.renderValues();
                                     ImGui.separator();
-                                    if (!values.isEmpty()) {
-                                        ImGui.text("Config");
-                                        ImGui.separator();
-                                        if (ImGui.button("Reset config" + moduleIdentifier + "resetconfigbutton")) {
-                                            for (final Value<?> value : values) {
-                                                value.resetValue();
-                                            }
-                                        }
-                                        module.renderValues();
-                                        ImGui.separator();
-                                    }
-                                    ImGui.endPopup();
                                 }
+                                ImGui.endPopup();
                             }
                         }
                         ImGui.endChild();
