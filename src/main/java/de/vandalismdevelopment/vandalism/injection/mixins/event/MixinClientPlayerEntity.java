8d673c1fa65e21fc2faa41ea8e6c681256423e78
NekosAreKawaii <admin@nekosarekawaii.de>
2023-12-01 05:24:34 +0100

    Added sprint event.

---
+++ src/main/java/de/vandalismdevelopment/vandalism/injection/mixins/event/MixinClientPlayerEntity.java
@@ -0,0 +1,28 @@
+package de.vandalismdevelopment.vandalism.injection.mixins.event;
+
+import de.florianmichael.dietrichevents2.DietrichEvents2;
+import de.vandalismdevelopment.vandalism.event.SprintListener;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.option.KeyBinding;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = ClientPlayerEntity.class, priority = 9999)
+public abstract class MixinClientPlayerEntity {
+
+    @Redirect(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;isPressed()Z", ordinal = 0))
+    private boolean vandalism$callSprintEvent1(final KeyBinding instance) {
+        final SprintListener.SprintEvent sprintEvent = new SprintListener.SprintEvent(instance.isPressed());
+        DietrichEvents2.global().postInternal(SprintListener.SprintEvent.ID, sprintEvent);
+        return sprintEvent.sprinting;
+    }
+
+    @Redirect(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;isPressed()Z", ordinal = 1))
+    private boolean vandalism$callSprintEvent2(final KeyBinding instance) {
+        final SprintListener.SprintEvent sprintEvent = new SprintListener.SprintEvent(instance.isPressed());
+        DietrichEvents2.global().postInternal(SprintListener.SprintEvent.ID, sprintEvent);
+        return sprintEvent.sprinting;
+    }
+
+}
