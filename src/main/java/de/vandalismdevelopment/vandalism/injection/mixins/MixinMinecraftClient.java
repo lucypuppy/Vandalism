f7eebdfc5fc92be36598444736693e378cffc7ac
NekosAreKawaii <admin@nekosarekawaii.de>
2023-12-02 16:36:15 +0100

    Added startTime to the game main for debugging purposes.

--- src/main/java/de/vandalismdevelopment/vandalism/injection/mixins/MixinMinecraftClient.java
+++ src/main/java/de/vandalismdevelopment/vandalism/injection/mixins/MixinMinecraftClient.java
@@ -6,6 +6,7 @@ import net.minecraft.client.util.Window;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -23,9 +24,24 @@ public abstract class MixinMinecraftClient {
     @Final
     public File runDirectory;
 
+    @Unique
+    private long startTime = 0L;
+
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/io/File;toPath()Ljava/nio/file/Path;", ordinal = 0))
+    private void vandalism$startTimeCalculation1(final CallbackInfo ci) {
+        startTime = System.currentTimeMillis();
+    }
+
     @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;onResolutionChanged()V"))
     private void vandalism$startMod(final CallbackInfo ci) {
         Vandalism.getInstance().start(this.window, this.runDirectory);
+    }
+
+    @Inject(method = "<init>", at = @At(value = "RETURN"))
+    private void vandalism$startTimeCalculation2(final CallbackInfo ci) {
+        Vandalism.getInstance().getLogger().info("");
+        Vandalism.getInstance().getLogger().info("Minecraft loading took " + (System.currentTimeMillis() - startTime) + "ms.");
+        Vandalism.getInstance().getLogger().info("");
     }
 
     @Inject(method = "updateWindowTitle", at = @At("HEAD"), cancellable = true)
