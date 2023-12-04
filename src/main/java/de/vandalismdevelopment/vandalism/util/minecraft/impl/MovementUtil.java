d74a1599c16c60ad9450aebc9de7899303b30ae1
xkeksi <74504314+xkeksi@users.noreply.github.com>
2023-12-04 04:03:45 +0100

    silent movefi, rotation system fixes

--- src/main/java/de/vandalismdevelopment/vandalism/util/minecraft/impl/MovementUtil.java
+++ src/main/java/de/vandalismdevelopment/vandalism/util/minecraft/impl/MovementUtil.java
@@ -1,9 +1,13 @@
 package de.vandalismdevelopment.vandalism.util.minecraft.impl;
 
+import de.vandalismdevelopment.vandalism.util.MathUtil;
 import de.vandalismdevelopment.vandalism.util.minecraft.MinecraftUtil;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 
 public class MovementUtil extends MinecraftUtil {
 
+    private static float[] INPUTS = new float[2];
     public static double getDirection() {
         if (player() == null) return 0;
         return (Math.atan2(player().forwardSpeed, player().sidewaysSpeed) / Math.PI * 180.0F + player().getYaw()) * Math.PI / 180.0F;
@@ -19,6 +23,75 @@ public class MovementUtil extends MinecraftUtil {
         if (player() == null) return;
         final double direction = getDirection();
         player().setPos(player().getX() - Math.sin(direction) * horizontal, player().getY() + vertical, player().getZ() + Math.cos(direction) * horizontal);
+    }
+
+    public static float[] getFixedMoveInputs(float rotationYaw, float speed){
+        INPUTS = new float[2];
+        if (Math.abs(player().forwardSpeed) > 0F || Math.abs(player().sidewaysSpeed) > 0F) {
+            final float wantedYaw = getInputAngle(player().getYaw());
+            Vec3d movementInput = new Vec3d(player().forwardSpeed, player().upwardSpeed, player().sidewaysSpeed);
+            final float currentDX = MathHelper.sin(rotationYaw * MathUtil.DEG_TO_RAD);
+            final float currentDZ = MathHelper.cos(rotationYaw * MathUtil.DEG_TO_RAD);
+            // .. as seen here
+            float currentBestForward = 1.0F;
+            float currentBestStrafing = 0.0F;
+            // the current best difference between any found combination and wantedYaw,
+            // initialized to be 180 because that's the maximum value a difference can
+            // be after being passed through MathHelper::wrapAngleToA18_float
+            float currentBestDiff = Float.MAX_VALUE;
+            // use this and not just hardcode 0.98F, because moveForward and moveStrafing
+            // is also dependent on whether the player is sneaking or using an item
+            float mag = Math.max(Math.abs(player().forwardSpeed), Math.abs(player().sidewaysSpeed));
+            // loop through all possible combinations of player.moveForward and player.moveStrafing
+            for (float forward : MathUtil.POSSIBLE_MOVEMENTS) {
+                for (float strafing : MathUtil.POSSIBLE_MOVEMENTS) {
+                    // don't do anything when the combination would make the player stand still
+                    // (this would fuck up sin and cos)
+                    if (forward == 0.0F && strafing == 0.0F) continue;
+
+                    movementInput = new Vec3d(forward, player().upwardSpeed, strafing);
+                   // Vec3d vec3d = (d > 1.0 ? movementInput.normalize() : movementInput).multiply((double)speed);
+                    // motionX and motionZ the player would have with
+                    // the current combination of moveForward and moveStrafing
+                    //vec3d.x * (double)g - vec3d.z * (double)f
+                    final float mX = (float) (movementInput.x * currentDZ - movementInput.z * currentDX);
+                    //vec3d.z * (double)g + vec3d.x * (double)f
+                    final float mZ = (float) (movementInput.z * currentDZ + movementInput.x * currentDX);
+                    // the yaw angle relative to the players rotation this
+                    // motion would make the player walk towards
+                    //   mc.thePlayer.addChatMessage(new ChatComponentText("" + (180.0 / Math.PI)));
+                    final float angle = (float) (Math.atan2(mZ, mX) * 180.0D / Math.PI - 90.0F);
+                    // ... and the difference of it to the wanted yaw relative to the player
+                    final float diff = Math.abs(MathUtil.wrapAngleTo180_float(angle - wantedYaw));
+                    // set combination, if signed distance between
+                    // wantedYaw and angle is lower then the last best difference
+                    if (diff < currentBestDiff) {
+                        currentBestForward = forward;
+                        currentBestStrafing = strafing;
+                        currentBestDiff = diff;
+                    }
+                }
+            }
+            INPUTS[0] = (currentBestForward * mag);
+            INPUTS[1] = (currentBestStrafing * mag);
+        }
+        return INPUTS;
+    }
+    public static float getInputAngle(float yaw) {
+        float vertical = player().forwardSpeed;
+        float horizontal = player().sidewaysSpeed;
+        if (vertical > 0) {
+            if (horizontal > 0) return yaw - 45F;
+            else if (horizontal < 0) return yaw + 45F;
+            else return yaw;
+        } else if (vertical < 0) {
+            if (horizontal > 0) return yaw - 135F;
+            else if (horizontal < 0) return yaw + 135F;
+            else return MathUtil.wrapAngleTo180_float(yaw - 180f);
+        }
+        if (horizontal > 0) return yaw - 90f;
+        else if (horizontal < 0) return yaw + 90f;
+        return yaw;
     }
 
 }
