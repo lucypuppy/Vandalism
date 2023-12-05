909fdcc0c963d6e5ff61c12e1d707ee0607dbb8e
xkeksi <74504314+xkeksi@users.noreply.github.com>
2023-12-05 05:33:34 +0100

    Extended World Utilities

--- src/main/java/de/vandalismdevelopment/vandalism/util/minecraft/impl/WorldUtil.java
+++ src/main/java/de/vandalismdevelopment/vandalism/util/minecraft/impl/WorldUtil.java
@@ -1,6 +1,7 @@
 package de.vandalismdevelopment.vandalism.util.minecraft.impl;
 
 import de.vandalismdevelopment.vandalism.util.minecraft.MinecraftUtil;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
@@ -30,6 +31,11 @@ public class WorldUtil extends MinecraftUtil {
         final Vec3d currentPos = playerPosition.add(lookDirection.x * maxDistance, lookDirection.y * maxDistance, lookDirection.z * maxDistance);
         final BlockHitResult rayTraceResult = world().raycast(new RaycastContext(playerPosition, currentPos, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, player()));
         return rayTraceResult == null || rayTraceResult.getType() != HitResult.Type.BLOCK;
+    }
+
+    public static double getPlayerEyeVectorDistance(PlayerEntity player, Vec3d rotationVector){
+        final Vec3d eyes = player.getEyePos();
+        return eyes.distanceTo(rotationVector);
     }
 
 }
