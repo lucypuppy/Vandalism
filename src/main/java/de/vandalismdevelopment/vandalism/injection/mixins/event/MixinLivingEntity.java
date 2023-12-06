4f91bd7f97ce60de7f4ef6ffeeb8398a0e9ddf27
NekosAreKawaii <admin@nekosarekawaii.de>
2023-12-06 06:00:19 +0100

    Skill issue fixed.

--- src/main/java/de/vandalismdevelopment/vandalism/injection/mixins/event/MixinLivingEntity.java
+++ src/main/java/de/vandalismdevelopment/vandalism/injection/mixins/event/MixinLivingEntity.java
@@ -2,6 +2,7 @@ package de.vandalismdevelopment.vandalism.injection.mixins.event;
 
 import de.florianmichael.dietrichevents2.DietrichEvents2;
 import de.vandalismdevelopment.vandalism.event.MovementListener;
+import de.vandalismdevelopment.vandalism.util.minecraft.MinecraftWrapper;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,10 +10,11 @@ import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(LivingEntity.class)
-public abstract class MixinLivingEntity {
+public abstract class MixinLivingEntity implements MinecraftWrapper {
 
     @ModifyArgs(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Vec3d;<init>(DDD)V"))
     private void vandalism$callMoveFlyingEvent(final Args args) {
+        if (this.player() != ((LivingEntity) (Object) this)) return;
         final double sidewaysSpeed = args.get(0), upwardSpeed = args.get(1), forwardSpeed = args.get(2);
         final MovementListener.MoveFlyingEvent moveFlyingEvent = new MovementListener.MoveFlyingEvent(sidewaysSpeed, upwardSpeed, forwardSpeed);
         DietrichEvents2.global().postInternal(MovementListener.MoveFlyingEvent.ID, moveFlyingEvent);
