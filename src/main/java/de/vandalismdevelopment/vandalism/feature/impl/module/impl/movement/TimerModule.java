b349ba607c03ff80c93f8455bf6a9494c1ce775b
Lucy <117228413+cuddlycatgirl@users.noreply.github.com>
2023-12-07 17:16:04 +0100

    Added timer (#8)
    * Added TimerModule and TimerUtil
    
    * Added my name to the authors.
    
    * Removed instance in the TimerUtil and player check in TimerModule.
    
    * Fixed an issue that I introduced in the last commit by accident.

---
+++ src/main/java/de/vandalismdevelopment/vandalism/feature/impl/module/impl/movement/TimerModule.java
@@ -0,0 +1,61 @@
+package de.vandalismdevelopment.vandalism.feature.impl.module.impl.movement;
+
+import de.florianmichael.dietrichevents2.DietrichEvents2;
+import de.vandalismdevelopment.vandalism.event.TickListener;
+import de.vandalismdevelopment.vandalism.feature.FeatureCategory;
+import de.vandalismdevelopment.vandalism.feature.impl.module.Module;
+import de.vandalismdevelopment.vandalism.util.minecraft.impl.player.TimerUtil;
+import de.vandalismdevelopment.vandalism.value.Value;
+import de.vandalismdevelopment.vandalism.value.impl.BooleanValue;
+import de.vandalismdevelopment.vandalism.value.impl.number.slider.SliderFloatValue;
+
+public class TimerModule extends Module implements TickListener {
+
+    public TimerModule() {
+        super(
+                "Timer",
+                "Modifies the timer speed.",
+                FeatureCategory.MOVEMENT,
+                false,
+                false
+        );
+    }
+
+    private final Value<Float> timerModifier = new SliderFloatValue(
+            "Timer Modifier",
+            "Allows you to customize the timer speed.",
+            this,
+            2.f,
+            .1f,
+            20.f
+    );
+
+    private final Value<Boolean> screen = new BooleanValue(
+            "Screen",
+            "Allows you to use the timer in screen(s).",
+            this,
+            false
+    );
+
+    @Override
+    public void onTick() {
+        if(!screen.getValue() && currentScreen() != null) {
+            TimerUtil.setSpeed(1.f);
+            return;
+        }
+
+        TimerUtil.setSpeed(timerModifier.getValue());
+    }
+
+    @Override
+    public void onEnable() {
+        DietrichEvents2.global().subscribe(TickEvent.ID, this);
+    }
+
+    @Override
+    public void onDisable() {
+        TimerUtil.setSpeed(1.f);
+        DietrichEvents2.global().unsubscribe(TickEvent.ID, this);
+    }
+
+}
