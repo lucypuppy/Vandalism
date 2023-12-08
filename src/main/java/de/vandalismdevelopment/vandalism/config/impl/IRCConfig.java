5586c4499561118870b788eecb5a67cb2feadb87
NekosAreKawaii <admin@nekosarekawaii.de>
2023-12-08 19:55:40 +0100

    Little cleanup.

--- src/main/java/de/vandalismdevelopment/vandalism/config/impl/IRCConfig.java
+++ src/main/java/de/vandalismdevelopment/vandalism/config/impl/IRCConfig.java
@@ -9,28 +9,28 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 
-public class IrcConfig extends ValueableConfig {
+public class IRCConfig extends ValueableConfig {
 
     private final HashMap<String, ImString> values = new HashMap<>();
 
-    public IrcConfig(final File configDir) {
+    public IRCConfig(final File configDir) {
         super(configDir, "irc");
 
-        values.put("address", IrcImGuiMenu.ADDRESS);
-        values.put("username", IrcImGuiMenu.USERNAME);
-        values.put("password", IrcImGuiMenu.PASSWORD);
+        this.values.put("address", IrcImGuiMenu.ADDRESS);
+        this.values.put("username", IrcImGuiMenu.USERNAME);
+        this.values.put("password", IrcImGuiMenu.PASSWORD);
     }
 
     @Override
     public JsonObject save() throws IOException {
         final JsonObject object = new JsonObject();
-        values.forEach((key, value) -> object.addProperty(key, value.get()));
+        this.values.forEach((key, value) -> object.addProperty(key, value.get()));
         return object;
     }
 
     @Override
     public void load(final JsonObject jsonObject) throws IOException {
-        values.forEach((key, value) -> {
+        this.values.forEach((key, value) -> {
             if (jsonObject.has(key))
                 value.set(jsonObject.get(key).getAsString());
         });
