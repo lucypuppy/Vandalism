f3101ba2607076bf41c74cd418ae9a6708a699dc
NekosAreKawaii <admin@nekosarekawaii.de>
2023-12-09 00:23:23 +0100

    Added vehicle one hit module.

---
+++ src/main/java/de/vandalismdevelopment/vandalism/injection/mixins/event/MixinClientPlayerInteractionManager.java
@@ -0,0 +1,21 @@
+package de.vandalismdevelopment.vandalism.injection.mixins.event;
+
+import de.florianmichael.dietrichevents2.DietrichEvents2;
+import de.vandalismdevelopment.vandalism.event.AttackListener;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public abstract class MixinClientPlayerInteractionManager {
+
+    @Inject(method = "attackEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerInteractionManager;syncSelectedSlot()V", shift = At.Shift.AFTER))
+    private void hookAttack(final PlayerEntity player, final Entity target, final CallbackInfo ci) {
+        DietrichEvents2.global().postInternal(AttackListener.AttackSendEvent.ID, new AttackListener.AttackSendEvent(target));
+    }
+
+}
