baaf29113ad334171ed6b0caaedbb5585f8645a7
xkeksi <74504314+xkeksi@users.noreply.github.com>
2023-12-12 06:02:00 +0100

    cubecraft bypasses, some changes

---
+++ src/main/java/de/vandalismdevelopment/vandalism/util/minecraft/MinecraftConstants.java
@@ -0,0 +1,8 @@
+package de.vandalismdevelopment.vandalism.util.minecraft;
+
+public class MinecraftConstants {
+
+    //thos constant get's used in shitty anticheats with no proper collision handling, posY % const == 0 = onground True
+    public static double MAGIC_ON_GROUND_MODULO_FACTOR = 0.015625;
+
+}
