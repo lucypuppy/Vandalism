86c3225b18845e55f459e2c8691ceefd9ef58786
NekosAreKawaii <admin@nekosarekawaii.de>
2023-12-14 17:55:42 +0100

    Don't handle the escape key.

--- src/main/java/de/nekosarekawaii/vandalism/base/value/impl/awt/KeyBindValue.java
+++ src/main/java/de/nekosarekawaii/vandalism/base/value/impl/awt/KeyBindValue.java
@@ -58,7 +58,7 @@ public class KeyBindValue extends Value<Integer> implements KeyboardInputListene
 
     @Override
     public void onKey(final long window, final int key, final int scanCode, final int action, final int modifiers) {
-        if (key != GLFW.GLFW_KEY_UNKNOWN && action == GLFW.GLFW_PRESS) {
+        if (key != GLFW.GLFW_KEY_UNKNOWN && key != GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_PRESS) {
             this.finishInput();
             this.setValue(key);
         }
