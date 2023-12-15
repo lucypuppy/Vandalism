e8a1ba47062e6a7c8e48c246cec0eaadff27d4bd
Verschlxfene <152820108+verschlxfene@users.noreply.github.com>
2023-12-15 22:47:52 +0100

    Cleaned mixin

--- src/main/java/de/nekosarekawaii/vandalism/injection/mixins/module/MixinTextureUrlChecker.java
+++ src/main/java/de/nekosarekawaii/vandalism/injection/mixins/module/MixinTextureUrlChecker.java
@@ -1,7 +1,6 @@
 package de.nekosarekawaii.vandalism.injection.mixins.module;
 
 import com.mojang.authlib.yggdrasil.TextureUrlChecker;
-import de.florianmichael.rclasses.common.StringUtils;
 import de.nekosarekawaii.vandalism.Vandalism;
 import de.nekosarekawaii.vandalism.feature.module.impl.exploit.ExploitFixerModule;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,11 +13,10 @@ public abstract class MixinTextureUrlChecker {
 
     @Inject(method = "isAllowedTextureDomain", at = @At("HEAD"), cancellable = true)
     private static void hookExploitFixer(final String url, final CallbackInfoReturnable<Boolean> cir) {
-        final ExploitFixerModule exploitFixerModule = Vandalism.getInstance().getModuleManager().getExploitFixerModule();
+        final var exploitFixerModule = Vandalism.getInstance().getModuleManager().getExploitFixerModule();
+
         if (exploitFixerModule.isActive() && exploitFixerModule.blockInvalidTextureUrls.getValue()) {
-            final boolean startsWithUrl1 = StringUtils.startsWith(url, "https://" + ExploitFixerModule.CORRECT_TEXTURE_URL_START);
-            final boolean startsWithUrl2 = StringUtils.startsWith(url, "http://" + ExploitFixerModule.CORRECT_TEXTURE_URL_START);
-            if (!startsWithUrl1 && !startsWithUrl2) {
+            if (!url.toLowerCase().startsWith("https://" + ExploitFixerModule.CORRECT_TEXTURE_URL_START) && !url.toLowerCase().startsWith("http://" + ExploitFixerModule.CORRECT_TEXTURE_URL_START)) {
                 cir.setReturnValue(false);
             }
         }
