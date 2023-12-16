e35a688d373bb47214e66832d103a941719167ae
NekosAreKawaii <admin@nekosarekawaii.de>
2023-12-16 05:35:21 +0100

    Fixed multi mode values.

--- src/main/java/de/nekosarekawaii/vandalism/base/value/Value.java
+++ src/main/java/de/nekosarekawaii/vandalism/base/value/Value.java
@@ -11,20 +11,22 @@ public abstract class Value<V> implements IName {
     private final ValueParent parent;
     private final String name;
     private final String description;
-    private final V defaultValue;
 
+    private final V defaultValue;
     private V value;
     private BiConsumer<V, V> valueChangeConsumer;
     private BooleanSupplier visibleCondition;
 
     public Value(ValueParent parent, String name, String description, V defaultValue) {
+        this(parent, name, description, defaultValue, defaultValue);
+    }
+
+    public Value(ValueParent parent, String name, String description, V defaultValue, V value) {
         this.parent = parent;
         this.name = name;
         this.description = description;
         this.defaultValue = defaultValue;
-
-        this.value = defaultValue;
-
+        this.value = value;
         parent.getValues().add(this);
     }
 
@@ -34,7 +36,6 @@ public abstract class Value<V> implements IName {
             // Allows the event to change the value
             this.valueChangeConsumer.accept(oldValue, value);
         }
-
         this.value = value;
     }
 
@@ -78,6 +79,7 @@ public abstract class Value<V> implements IName {
     }
 
     public abstract void load(final JsonObject mainNode);
+
     public abstract void save(final JsonObject mainNode);
 
     public abstract void render();
