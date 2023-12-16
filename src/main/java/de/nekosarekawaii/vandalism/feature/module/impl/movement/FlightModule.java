1d235b0bf398a5cc66d4cf3700e83cac89a7bc56
FooFieOwO <foofieowo@gmail.com>
2023-12-16 13:10:08 +0100

    fixed antikick bypass. (Tested localhost please report issues to @FooFieOwO)

--- src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/movement/FlightModule.java
+++ src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/movement/FlightModule.java
@@ -1,6 +1,7 @@
 package de.nekosarekawaii.vandalism.feature.module.impl.movement;
 
 import de.florianmichael.dietrichevents2.DietrichEvents2;
+import de.nekosarekawaii.vandalism.base.event.entity.MotionListener;
 import de.nekosarekawaii.vandalism.base.event.network.OutgoingPacketListener;
 import de.nekosarekawaii.vandalism.base.value.impl.primitive.BooleanValue;
 import de.nekosarekawaii.vandalism.feature.module.AbstractModule;
@@ -9,7 +10,7 @@ import de.nekosarekawaii.vandalism.feature.module.impl.movement.modes.flight.Cub
 import de.nekosarekawaii.vandalism.feature.module.template.ModuleModeValue;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
-public class FlightModule extends AbstractModule implements OutgoingPacketListener {
+public class FlightModule extends AbstractModule implements OutgoingPacketListener, MotionListener {
 
     private final BooleanValue antiKick = new BooleanValue(
             this,
@@ -26,24 +27,40 @@ public class FlightModule extends AbstractModule implements OutgoingPacketListen
             new CubeCraftModuleMode(this)
     );
 
+    private double lastPacketY = 0.0D;
+
     public FlightModule() {
         super("Flight", "Allows you to fly (even in survival or adventure).", Category.MOVEMENT);
     }
 
     @Override
     public void onEnable() {
+        DietrichEvents2.global().subscribe(MotionListener.MotionEvent.ID, this);
         DietrichEvents2.global().subscribe(OutgoingPacketEvent.ID, this);
     }
 
     @Override
     public void onDisable() {
+        DietrichEvents2.global().unsubscribe(MotionListener.MotionEvent.ID, this);
         DietrichEvents2.global().unsubscribe(OutgoingPacketEvent.ID, this);
+    }
+
+
+    @Override
+    public void onPreMotion(MotionEvent event) {
+        if (this.mc.player == null) return;
+
+        if (this.antiKick.getValue()) {
+            this.mc.player.ticksSinceLastPositionPacketSent = 20;
+        }
     }
 
     @Override
     public void onOutgoingPacket(final OutgoingPacketEvent event) {
-        if (event.packet instanceof final PlayerMoveC2SPacket playerMoveC2SPacket && this.antiKick.getValue()) {
-            playerMoveC2SPacket.y += Math.sin(this.mc.player.age) * 0.2;
+        if (event.packet instanceof final PlayerMoveC2SPacket playerMoveC2SPacket
+                && this.antiKick.getValue()
+                && this.mc.player.age % 2 == 0) {
+            playerMoveC2SPacket.y = playerMoveC2SPacket.y - 0.1D;
         }
     }
 
