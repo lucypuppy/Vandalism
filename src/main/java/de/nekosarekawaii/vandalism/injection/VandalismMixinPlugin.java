6a62ab364ee76f8761290201c932f9b031228f61
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2023-12-23 23:55:21 +0100

    Wurst mixin fix.

---
+++ src/main/java/de/nekosarekawaii/vandalism/injection/VandalismMixinPlugin.java
@@ -0,0 +1,46 @@
+package de.nekosarekawaii.vandalism.injection;
+
+import net.lenni0451.reflect.stream.RStream;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class VandalismMixinPlugin implements IMixinConfigPlugin {
+
+    @Override
+    public void onLoad(String mixinPackage) {
+        final Set<String> loadedMixins = RStream.of("org.spongepowered.asm.mixin.transformer.MixinConfig").fields().by("globalMixinList").get();
+        loadedMixins.add("net.wurstclient.mixin.GameMenuScreenMixin");
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+}
