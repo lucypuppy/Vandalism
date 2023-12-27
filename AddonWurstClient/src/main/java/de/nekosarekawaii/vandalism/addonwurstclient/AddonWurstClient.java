b76875b25976dcedb06909d3e7d37a797f512c1b
Verschlxfene <152820108+verschlxfene@users.noreply.github.com>
2023-12-27 03:00:26 +0100

    Fixed Wurst module <-> enabled bool integration

--- AddonWurstClient/src/main/java/de/nekosarekawaii/vandalism/addonwurstclient/AddonWurstClient.java
+++ AddonWurstClient/src/main/java/de/nekosarekawaii/vandalism/addonwurstclient/AddonWurstClient.java
@@ -7,11 +7,18 @@ import net.wurstclient.WurstClient;
 
 public class AddonWurstClient implements VandalismAddonLauncher {
 
+    private WurstClientModule module;
+
     @Override
     public void onLaunch(Vandalism vandalism) {
         WurstClient.INSTANCE.initialize(); // Initialize WurstClient, counterpart in MixinWurstInitializer.java
 
-        vandalism.getModuleManager().add(new WurstClientModule());
+        vandalism.getModuleManager().add(module = new WurstClientModule());
+    }
+
+    @Override
+    public void onLateLaunch(Vandalism vandalism) {
+        WurstClient.INSTANCE.setEnabled(module.isActive());
     }
 
 }
