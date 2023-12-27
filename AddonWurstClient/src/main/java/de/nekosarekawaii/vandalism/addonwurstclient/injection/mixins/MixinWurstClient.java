2a2b8d5bca8622e48649bcaa44a737a6a11d9e19
Lucy Luna <117228413+cuddlycatgirl@users.noreply.github.com>
2023-12-27 17:19:22 +0100

    Wurst addon crash fix

--- AddonWurstClient/src/main/java/de/nekosarekawaii/vandalism/addonwurstclient/injection/mixins/MixinWurstClient.java
+++ AddonWurstClient/src/main/java/de/nekosarekawaii/vandalism/addonwurstclient/injection/mixins/MixinWurstClient.java
@@ -2,9 +2,12 @@ package de.nekosarekawaii.vandalism.addonwurstclient.injection.mixins;
 
 import net.minecraft.client.option.KeyBinding;
 import net.wurstclient.WurstClient;
+import net.wurstclient.analytics.WurstAnalytics;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.nio.file.Path;
 
 @Mixin(value = WurstClient.class, remap = false)
 public abstract class MixinWurstClient {
@@ -14,6 +17,11 @@ public abstract class MixinWurstClient {
         // Prevent the FAPI from loading the keybinding as we are loading the WurstClient to late for the FAPI
         // Since we don't need this keybinding anyway, we can just get rid of it
         return keyBinding;
+    }
+
+    @Redirect(method = "initialize", at = @At(value = "INVOKE", target = "Lnet/wurstclient/analytics/WurstAnalytics;trackPageView(Ljava/lang/String;Ljava/lang/String;)V", remap = false))
+    private void ignoreTrackPageView(final WurstAnalytics instance, final String url, final String title) {
+        // Prevents the client from crashing...
     }
 
 }
