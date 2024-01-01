044d262cf03eef29a92e4749e96e9b4801b685e1
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2024-01-01 23:58:56 +0100

    More fixes for the wurst client. This will allow you to open the wurst options screen via OptionsHack. This removes the zoom and the count users button in the wurst options.

--- AddonWurstClient/src/main/java/de/nekosarekawaii/vandalism/addonwurstclient/injection/mixins/common/MixinHackList.java
+++ AddonWurstClient/src/main/java/de/nekosarekawaii/vandalism/addonwurstclient/injection/mixins/common/MixinHackList.java
@@ -1,15 +1,31 @@
 package de.nekosarekawaii.vandalism.addonwurstclient.injection.mixins.common;
 
+import de.nekosarekawaii.vandalism.addonwurstclient.hack.WurstClientOptionsHack;
+import net.wurstclient.hack.Hack;
 import net.wurstclient.hack.HackList;
 import net.wurstclient.hacks.HealthTagsHack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.nio.file.Path;
 import java.util.TreeMap;
 
 @Mixin(value = HackList.class, remap = false)
 public abstract class MixinHackList {
+
+    @Shadow
+    @Final
+    private TreeMap<String, Hack> hax;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void addSomeWurstHacks(final Path enabledHacksFile, final CallbackInfo ci) {
+        this.hax.put("Options", new WurstClientOptionsHack());
+    }
 
     @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/util/TreeMap;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"))
     private Object removeSomeWurstHacks(TreeMap instance, Object key, Object value) {
