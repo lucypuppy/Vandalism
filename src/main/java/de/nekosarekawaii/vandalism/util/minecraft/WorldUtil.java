0fe0b8f306aaf0e555793437ac1c589813e82f7d
FooFieOwO <foofieowo@gmail.com>
2024-01-03 01:46:03 +0100

    fixed a bug.

--- src/main/java/de/nekosarekawaii/vandalism/util/minecraft/WorldUtil.java
+++ src/main/java/de/nekosarekawaii/vandalism/util/minecraft/WorldUtil.java
@@ -9,6 +9,7 @@ import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 
 public class WorldUtil implements MinecraftWrapper {
 
@@ -28,17 +29,19 @@ public class WorldUtil implements MinecraftWrapper {
 
     // This is a edited copy of net.minecraft.client.render.GameRenderer.updateTargetedEntity
     public static Pair<HitResult, Double> rayTrace(final Rotation rotation, final Vec3d cameraVec, final double range) {
-        final HitResult crosshairTarget = mc.player.raycast(range, 1.0f, false);
-        final double expectedRange = crosshairTarget != null ? crosshairTarget.getPos().distanceTo(cameraVec) : range;
-
         final Vec3d rotationVec = rotation.getVector();
         final Vec3d maxVec = cameraVec.add(rotationVec.x * range, rotationVec.y * range, rotationVec.z * range);
+
+        final HitResult crosshairTarget = mc.player.getWorld().raycast(new RaycastContext(cameraVec, maxVec,
+                RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, mc.player));
+        final double expectedRange = crosshairTarget != null ? crosshairTarget.getPos().distanceTo(cameraVec) : range;
+
         final Box box = mc.player.getBoundingBox().stretch(rotationVec.multiply(range)).expand(1.0F);
         final EntityHitResult entityHitResult = ProjectileUtil.raycast(mc.player, cameraVec, maxVec, box,
                 entity -> !entity.isSpectator() && entity.canHit(), Math.pow(expectedRange, 2));
 
         if (entityHitResult != null) { // Future lilly & keksbye find out why we need that offset im too tired now.
-            final double raytraceRange = entityHitResult.getPos().distanceTo(cameraVec) + 0.23989532614;
+            final double raytraceRange = entityHitResult.getPos().distanceTo(cameraVec) + 0.24;
 
             if (raytraceRange < expectedRange || crosshairTarget == null) {
                 //ChatUtil.chatMessage("b " + raytraceRange + " < " + expectedRange);
