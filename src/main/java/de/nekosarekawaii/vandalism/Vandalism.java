9fc2c9cc997898e63faf1eea3c24534aee9d1912
FooFieOwO <foofieowo@gmail.com>
2024-01-07 15:01:13 +0100

    added new target system.

--- src/main/java/de/nekosarekawaii/vandalism/Vandalism.java
+++ src/main/java/de/nekosarekawaii/vandalism/Vandalism.java
@@ -74,7 +74,7 @@ public class Vandalism implements MinecraftBoostrapListener, ShutdownProcessList
 
     private static final Vandalism instance = new Vandalism();
 
-    private final DietrichEvents2 eventSystem = new DietrichEvents2(34 /* This value has to be incremented for every new event */, Throwable::printStackTrace);
+    private final DietrichEvents2 eventSystem = new DietrichEvents2(35 /* This value has to be incremented for every new event */, Throwable::printStackTrace);
     private final Logger logger = LoggerFactory.getLogger(FabricBootstrap.MOD_NAME);
 
     // Base handlers
