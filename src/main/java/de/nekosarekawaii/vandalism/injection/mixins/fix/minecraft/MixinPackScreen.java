546b4b959290d8bc2254b8199b9625599f0ca316
NekosAreKawaii <admin@nekosarekawaii.de>
2024-01-29 05:11:58 +0100

    Fixed pack screen background.

---
+++ src/main/java/de/nekosarekawaii/vandalism/injection/mixins/fix/minecraft/MixinPackScreen.java
@@ -0,0 +1,47 @@
+/*
+ * This file is part of Vandalism - https://github.com/VandalismDevelopment/Vandalism
+ * Copyright (C) 2023-2024 NekosAreKawaii, Verschlxfene, FooFieOwO and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.nekosarekawaii.vandalism.injection.mixins.fix.minecraft;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.pack.PackScreen;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(PackScreen.class)
+public abstract class MixinPackScreen extends Screen {
+
+    protected MixinPackScreen(final Text title) {
+        super(title);
+    }
+
+    @Redirect(method = "renderBackground", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/pack/PackScreen;renderBackgroundTexture(Lnet/minecraft/client/gui/DrawContext;)V"))
+    private void fixBackground(final PackScreen instance, final DrawContext drawContext) {
+        if (this.client != null) {
+            if (this.client.world != null) {
+                this.renderInGameBackground(drawContext);
+            } else {
+                this.renderBackgroundTexture(drawContext);
+            }
+        }
+    }
+
+}
