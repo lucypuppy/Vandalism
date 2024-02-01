ac1b9ecd2a126b480225906627047e240710297a
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2024-02-01 01:40:23 +0100

    Fixed packet manager module config

--- src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/misc/PacketManagerModule.java
+++ src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/misc/PacketManagerModule.java
@@ -23,10 +23,8 @@ import de.florianmichael.rclasses.common.StringUtils;
 import de.nekosarekawaii.vandalism.Vandalism;
 import de.nekosarekawaii.vandalism.base.event.cancellable.network.IncomingPacketListener;
 import de.nekosarekawaii.vandalism.base.event.cancellable.network.OutgoingPacketListener;
-import de.nekosarekawaii.vandalism.base.value.Value;
 import de.nekosarekawaii.vandalism.base.value.impl.primitive.BooleanValue;
 import de.nekosarekawaii.vandalism.base.value.impl.selection.MultiModeValue;
-import de.nekosarekawaii.vandalism.base.value.template.ValueGroup;
 import de.nekosarekawaii.vandalism.feature.module.AbstractModule;
 import de.nekosarekawaii.vandalism.util.game.ChatUtil;
 import net.minecraft.network.NetworkSide;
@@ -61,20 +59,24 @@ public class PacketManagerModule extends AbstractModule implements IncomingPacke
         final List<String> clientPackets = new ArrayList<>();
         for (final NetworkState networkState : NetworkState.values()) {
             final String networkStateName = StringUtils.normalizeEnumName(networkState.name());
-            networkState.getPacketIdToPacketMap(NetworkSide.CLIENTBOUND).values().stream().map(p -> networkStateName + p.getSimpleName()).forEachOrdered(serverPackets::add);
-            networkState.getPacketIdToPacketMap(NetworkSide.SERVERBOUND).values().stream().map(p -> networkStateName + p.getSimpleName()).forEachOrdered(clientPackets::add);
+            for (final Class<? extends Packet<?>> serverPacketClass : networkState.getPacketIdToPacketMap(NetworkSide.CLIENTBOUND).values()) {
+                serverPackets.add(networkStateName + serverPacketClass.getSimpleName());
+            }
+            for (final Class<? extends Packet<?>> clientPacketClass : networkState.getPacketIdToPacketMap(NetworkSide.SERVERBOUND).values()) {
+                clientPackets.add(networkStateName + clientPacketClass.getSimpleName());
+            }
         }
         this.serverPackets = new MultiModeValue(
                 this,
                 "Server Packets",
                 "The incoming packets.",
-                serverPackets
+                serverPackets.toArray(new String[serverPackets.size()])
         );
         this.clientPackets = new MultiModeValue(
                 this,
                 "Client Packets",
                 "The outgoing packets.",
-                clientPackets
+                clientPackets.toArray(new String[clientPackets.size()])
         );
     }
 
@@ -90,69 +92,24 @@ public class PacketManagerModule extends AbstractModule implements IncomingPacke
         Vandalism.getInstance().getEventSystem().unsubscribe(OutgoingPacketEvent.ID, this);
     }
 
-    private boolean loggedContainsPacket(final boolean outgoing, final Packet<?> packet, final NetworkState networkState) {
-        if (!this.log.getValue()) {
-            return false;
-        }
-        final String packetName = packet.getClass().getSimpleName();
-        for (final Value<?> value : this.getValues()) {
-            if (value instanceof final ValueGroup valueGroup && valueGroup.getName().equalsIgnoreCase(networkState.name())) {
-                for (final Value<?> valueGroupValue : valueGroup.getValues()) {
-                    if (valueGroupValue instanceof final MultiModeValue multiModeValue) {
-                        if (multiModeValue.getValue().contains(packetName)) {
-                            if (outgoing) {
-                                return multiModeValue.equals(this.clientPackets);
-                            } else {
-                                return multiModeValue.equals(this.serverPackets);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    private boolean cancelContainsPacket(final boolean outgoing, final Packet<?> packet, final NetworkState networkState) {
-        if (!this.cancel.getValue()) {
-            return false;
-        }
-        final String packetName = packet.getClass().getSimpleName();
-        for (final Value<?> value : this.getValues()) {
-            if (value instanceof final ValueGroup valueGroup && valueGroup.getName().equalsIgnoreCase(networkState.name())) {
-                for (final Value<?> valueGroupValue : valueGroup.getValues()) {
-                    if (valueGroupValue instanceof final MultiModeValue multiModeValue) {
-                        if (multiModeValue.getValue().contains(packetName)) {
-                            if (outgoing) {
-                                return multiModeValue.equals(this.clientPackets);
-                            } else {
-                                return multiModeValue.equals(this.serverPackets);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
     private boolean handlePacket(final boolean outgoing, final Packet<?> packet, final NetworkState networkState) {
-        final String packetName = packet.getClass().getSimpleName();
-        if (this.loggedContainsPacket(outgoing, packet, networkState)) {
+        final String name = StringUtils.normalizeEnumName(networkState.name()) + packet.getClass().getSimpleName();
+        final boolean contains = this.clientPackets.isSelected(name) || this.serverPackets.isSelected(name);
+        if (this.log.getValue() && contains) {
             final StringBuilder text = new StringBuilder();
             if (outgoing) {
                 text.append("Outgoing packet: ");
             } else {
                 text.append("Incoming packet: ");
             }
-            text.append(packetName);
+            text.append(name);
             if (this.mc.inGameHud != null) {
                 ChatUtil.infoChatMessage(text.toString());
             } else {
                 Vandalism.getInstance().getLogger().info(text.toString());
             }
         }
-        return this.cancelContainsPacket(outgoing, packet, networkState);
+        return this.cancel.getValue() && contains;
     }
 
     @Override
