b1f85732316f77a0805c401c9d3ad313b73c1187
Lucy Luna <117228413+cuddlycatgirl@users.noreply.github.com>
2024-02-01 13:58:18 +0100

    WTab -> WTap (The correct name..)

--- src/main/java/de/nekosarekawaii/vandalism/feature/module/ModuleManager.java
+++ src/main/java/de/nekosarekawaii/vandalism/feature/module/ModuleManager.java
@@ -134,7 +134,7 @@ public class ModuleManager extends NamedStorage<AbstractModule> implements
                 new VehicleFlightModule(),
                 new EcholocationModule(),
                 new AutoClickerModule(),
-                new WTabModule(),
+                new WTapModule(),
                 new CraftCarryModule()
         );
         this.configManager.add(new ConfigWithValues("modules", getList()));
