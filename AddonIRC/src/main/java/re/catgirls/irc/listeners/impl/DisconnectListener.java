c0c9d5d0e2e2f728c32fdf2ee732d2a840e3b5d7
NekosAreKawaii <admin@nekosarekawaii.de>
2024-02-13 17:25:40 +0100

    Removed two Addons because they don't work anymore

--- AddonIRC/src/main/java/re/catgirls/irc/listeners/impl/DisconnectListener.java
+++
@@ -1,5 +0,0 @@
-package re.catgirls.irc.listeners.impl;
-
-public interface DisconnectListener {
-    void onDisconnect();
-}
--- AddonServerDiscovery/src/main/java/re/catgirls/serverdiscovery/search/NumberRangeType.java
+++
@@ -1,9 +0,0 @@
-package re.catgirls.serverdiscovery.search;
-
-public enum NumberRangeType {
-    ANY,
-    EQUALS,
-    LESS_THAN,
-    GREATER_THAN,
-    BETWEEN;
-}
