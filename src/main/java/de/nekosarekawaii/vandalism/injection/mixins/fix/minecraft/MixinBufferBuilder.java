408956522479076ce1c8b857ba7e7ba0194b1e97
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2024-02-19 23:42:26 +0100

    Added IllegalStateException

---
+++ src/main/java/de/nekosarekawaii/vandalism/injection/mixins/fix/minecraft/MixinBufferBuilder.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of Vandalism - https://github.com/VandalismDevelopment/Vandalism
+ * Copyright (C) 2023-2024 NekosAreKawaii, Verschlxfene, FooFieOwO and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.nekosarekawaii.vandalism.injection.mixins.fix.minecraft;
+
+import net.minecraft.client.render.BufferBuilder;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BufferBuilder.class)
+public abstract class MixinBufferBuilder {
+
+    @Shadow private int elementOffset;
+
+    @Inject(method = "grow(I)V", at = @At("HEAD"))
+    private void forceThrowGrowLimitExceeded(final int size, final CallbackInfo ci) {
+        if (this.elementOffset > this.elementOffset + size) {
+            throw new IllegalStateException("BufferBuilder grow limit exceeded (size: " + size + ")");
+        }
+    }
+
+}
