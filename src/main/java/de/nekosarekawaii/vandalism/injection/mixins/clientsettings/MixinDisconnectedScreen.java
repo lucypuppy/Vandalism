b992c988c2e2627bd47b3443a2b09c372aba9884
NekosAreKawaii <admin@nekosarekawaii.de>
2024-02-19 10:07:21 +0100

    Added auto reconnect

--- src/main/java/de/nekosarekawaii/vandalism/injection/mixins/clientsettings/MixinDisconnectedScreen.java
+++ src/main/java/de/nekosarekawaii/vandalism/injection/mixins/clientsettings/MixinDisconnectedScreen.java
@@ -18,16 +18,22 @@
 
 package de.nekosarekawaii.vandalism.injection.mixins.clientsettings;
 
+import de.florianmichael.rclasses.math.timer.MSTimer;
 import de.nekosarekawaii.vandalism.Vandalism;
+import de.nekosarekawaii.vandalism.base.clientsettings.impl.MenuSettings;
 import de.nekosarekawaii.vandalism.util.game.ServerUtil;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.DisconnectedScreen;
+import net.minecraft.client.gui.screen.LoadingDisplay;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.*;
 import net.minecraft.text.Text;
+import net.minecraft.util.Util;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -37,6 +43,12 @@ public abstract class MixinDisconnectedScreen extends Screen {
     @Shadow
     @Final
     private Screen parent;
+
+    @Unique
+    private final MSTimer vandalism$reconnectTimer = new MSTimer();
+
+    @Unique
+    private long vandalism$oldAutoReconnectDelay = 0L;
 
     protected MixinDisconnectedScreen(final Text ignored) {
         super(ignored);
@@ -52,12 +64,71 @@ public abstract class MixinDisconnectedScreen extends Screen {
         return super.keyPressed(keyCode, scanCode, modifiers);
     }
 
+    @Override
+    public void tick() {
+        super.tick();
+        final MenuSettings menuSettings = Vandalism.getInstance().getClientSettings().getMenuSettings();
+        if (menuSettings.moreDisconnectedScreenButtons.getValue() && menuSettings.autoReconnect.getValue()) {
+            final long autoReconnectDelay = Math.max(menuSettings.autoReconnectDelay.getValue() + 1, 1) * 1000L;
+            if (this.vandalism$oldAutoReconnectDelay != autoReconnectDelay) {
+                this.vandalism$oldAutoReconnectDelay = autoReconnectDelay;
+                this.vandalism$reconnectTimer.reset();
+            }
+            if (this.vandalism$reconnectTimer.hasReached(autoReconnectDelay, true)) {
+                ServerUtil.connectToLastServer();
+            }
+        }
+        else {
+            this.vandalism$reconnectTimer.reset();
+        }
+    }
+
+    @Override
+    public void render(final DrawContext context, final int mouseX, final int mouseY, final float delta) {
+        super.render(context, mouseX, mouseY, delta);
+        final MenuSettings menuSettings = Vandalism.getInstance().getClientSettings().getMenuSettings();
+        if (menuSettings.moreDisconnectedScreenButtons.getValue() && menuSettings.autoReconnect.getValue()) {
+            final long countdownTime = Math.max(menuSettings.autoReconnectDelay.getValue() + 1, 1) * 1000L;
+            final long timerDelta = this.vandalism$reconnectTimer.getDelta();
+            final long remainingTime = (timerDelta < countdownTime ? countdownTime - timerDelta : 0) / 1000;
+            final int x = this.width / 2;
+            final int y = 15;
+            final int y2 = y + this.textRenderer.fontHeight + 2;
+            context.drawCenteredTextWithShadow(
+                    this.textRenderer,
+                    "Reconnecting in " + (remainingTime < 10 ? "0" + remainingTime : remainingTime),
+                    x,
+                    y,
+                    -1
+            );
+            context.drawCenteredTextWithShadow(this.textRenderer, LoadingDisplay.get(Util.getMeasuringTimeMs()), x, y2, -1);
+        }
+    }
+
+    @Override
+    public void removed() {
+        super.removed();
+        this.vandalism$reconnectTimer.reset();
+    }
+
+    @Override
+    public void close() {
+        super.close();
+        this.vandalism$reconnectTimer.reset();
+    }
+
     @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/DirectionalLayoutWidget;add(Lnet/minecraft/client/gui/widget/Widget;)Lnet/minecraft/client/gui/widget/Widget;", ordinal = 2))
     private <T extends Widget> T addMoreButtons(final DirectionalLayoutWidget instance, final T widget) {
         instance.add(widget);
-        if (Vandalism.getInstance().getClientSettings().getMenuSettings().moreDisconnectedScreenButtons.getValue()) {
+        this.vandalism$reconnectTimer.resume();
+        final MenuSettings menuSettings = Vandalism.getInstance().getClientSettings().getMenuSettings();
+        if (menuSettings.moreDisconnectedScreenButtons.getValue()) {
             final Positioner positioner = instance.getMainPositioner().copy().marginTop(-8);
             instance.add(ButtonWidget.builder(Text.literal("Reconnect"), button -> ServerUtil.connectToLastServer()).build(), positioner);
+            instance.add(ButtonWidget.builder(Text.literal("Auto Reconnect: " + (menuSettings.autoReconnect.getValue() ? "On" : "Off")), button -> {
+                menuSettings.autoReconnect.setValue(!menuSettings.autoReconnect.getValue());
+                button.setMessage(Text.literal("Auto Reconnect: " + (menuSettings.autoReconnect.getValue() ? "On" : "Off")));
+            }).build(), positioner);
             instance.add(ButtonWidget.builder(Text.literal("Copy Message"), button -> {
                 final StringBuilder textBuilder = new StringBuilder(ServerUtil.lastServerExists() ? "Disconnect Message from " + ServerUtil.getLastServerInfo().address : "");
                 final String emptyLine = "\n\n";
@@ -72,6 +143,11 @@ public abstract class MixinDisconnectedScreen extends Screen {
                 final String text = textBuilder.toString();
                 if (!text.isBlank()) {
                     this.client.keyboard.setClipboard(text);
+                }
+            }).build(), positioner);
+            instance.add(ButtonWidget.builder(Text.literal("Copy Address"), button -> {
+                if (ServerUtil.lastServerExists()) {
+                    this.client.keyboard.setClipboard(ServerUtil.getLastServerInfo().address);
                 }
             }).build(), positioner);
         }
