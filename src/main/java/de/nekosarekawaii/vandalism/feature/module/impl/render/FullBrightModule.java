33f8c5db7fef18a13089662f9de2845023e4b679
recyz <aze.hacks002@gmail.com>
2024-02-22 10:44:52 +0100

    Added FullBrightModule (still need to add gamma, only nightvision rn)

---
+++ src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/render/FullBrightModule.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of Vandalism - https://github.com/VandalismDevelopment/Vandalism
+ * Copyright (C) 2023-2024 NekosAreKawaii, Verschlxfene, FooFieOwO and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.nekosarekawaii.vandalism.feature.module.impl.render;
+
+import de.nekosarekawaii.vandalism.Vandalism;
+import de.nekosarekawaii.vandalism.base.event.normal.player.PlayerUpdateListener;
+import de.nekosarekawaii.vandalism.feature.module.AbstractModule;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+
+public class FullBrightModule extends AbstractModule implements PlayerUpdateListener {
+    public FullBrightModule() {
+        super("FullBright", "Makes your game brighter", Category.RENDER);
+    }
+
+    @Override
+    public void onActivate() {
+        Vandalism.getInstance().getEventSystem().subscribe(PlayerUpdateEvent.ID, this);
+    }
+
+    @Override
+    public void onDeactivate() {
+        Vandalism.getInstance().getEventSystem().unsubscribe(PlayerUpdateEvent.ID, this);
+        mc.player.removeStatusEffectInternal(StatusEffects.NIGHT_VISION);
+    }
+
+    @Override
+    public void onPostPlayerUpdate(PlayerUpdateEvent event) {
+        if (mc.player == null) return;
+        mc.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 1000000, 0, false, false, false));
+    }
+}
