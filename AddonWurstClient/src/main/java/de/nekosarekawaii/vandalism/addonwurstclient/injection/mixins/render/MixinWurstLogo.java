3e98bf21fa4dfd9627036dc6e9300d24b2572c96
Adrian <137268600+NekosAreKawaii@users.noreply.github.com>
2024-02-26 21:12:08 +0100

    Wurstlogofix (#38)
    * fixed box mueller clicker.
    
    * fixed wurst logo rect.
    
    ---------
    
    Co-authored-by: FooFieOwO <foofieowo@gmail.com>

--- AddonWurstClient/src/main/java/de/nekosarekawaii/vandalism/addonwurstclient/injection/mixins/render/MixinWurstLogo.java
+++ AddonWurstClient/src/main/java/de/nekosarekawaii/vandalism/addonwurstclient/injection/mixins/render/MixinWurstLogo.java
@@ -22,9 +22,11 @@ import de.nekosarekawaii.vandalism.Vandalism;
 import de.nekosarekawaii.vandalism.addonwurstclient.AddonWurstClient;
 import de.nekosarekawaii.vandalism.feature.hud.impl.WatermarkHUDElement;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import net.wurstclient.hud.WurstLogo;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -32,6 +34,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = WurstLogo.class)
 public abstract class MixinWurstLogo {
+
+    @Shadow
+    protected abstract void drawQuads(MatrixStack matrices, int x1, int y1, int x2, int y2, float r, float g, float b, float a);
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/wurstclient/hud/WurstLogo;drawQuads(Lnet/minecraft/client/util/math/MatrixStack;IIIIFFFF)V"))
+    private void cancelWurstLogoQuad(final WurstLogo instance, final MatrixStack matrices, final int x1, final int y1, final int x2, final int y2, final float r, final float g, final float b, final float a) {
+        final WatermarkHUDElement watermarkHUDElement = Vandalism.getInstance().getHudManager().watermarkHUDElement;
+        if (!watermarkHUDElement.isActive()) return;
+
+        final AddonWurstClient addonWurstClient = AddonWurstClient.getInstance();
+        final int width = 72;
+        final int height = 18;
+
+        this.drawQuads(matrices, watermarkHUDElement.getX() + addonWurstClient.wurstOffsetX.getValue(),
+                watermarkHUDElement.getY() + addonWurstClient.wurstOffsetY.getValue(),
+                watermarkHUDElement.getX() + addonWurstClient.wurstOffsetX.getValue() + width,
+                watermarkHUDElement.getY() + addonWurstClient.wurstOffsetY.getValue() + height,
+                r, g, b, a);
+    }
 
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIFFIIII)V"))
     private void moveWurstLogoDrawTexture(final DrawContext instance, final Identifier texture, final int x, final int y, final float u, final float v, final int width, final int height, final int textureWidth, final int textureHeight) {
