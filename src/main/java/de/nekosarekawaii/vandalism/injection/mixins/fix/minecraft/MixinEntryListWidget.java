b8a3344c8672c3750df7da0de81fb32231e10129
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2024-03-03 02:23:58 +0100

    Probably fixed ioobe

---
+++ src/main/java/de/nekosarekawaii/vandalism/injection/mixins/fix/minecraft/MixinEntryListWidget.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of Vandalism - https://github.com/VandalismDevelopment/Vandalism
+ * Copyright (C) 2023-2024 NekosAreKawaii, Verschlxfene, FooFieOwO and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.nekosarekawaii.vandalism.injection.mixins.fix.minecraft;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.widget.EntryListWidget;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(EntryListWidget.class)
+public abstract class MixinEntryListWidget {
+
+    @Shadow @Final private List<?> children;
+
+    @Inject(method = "renderEntry", at = @At("HEAD"), cancellable = true)
+    private void fixIOOBE(final DrawContext context, final int mouseX, final int mouseY, final float delta, final int index, final int x, final int y, final int entryWidth, final int entryHeight, final CallbackInfo ci) {
+        if (this.children.isEmpty()) {
+            ci.cancel();
+        }
+    }
+
+}
