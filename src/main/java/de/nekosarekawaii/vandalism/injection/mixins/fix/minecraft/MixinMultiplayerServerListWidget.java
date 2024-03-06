d1db369d995492a2cad732e7fb3984028540664d
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2024-03-06 23:55:57 +0100

    i don't want any more make it stop end it end me...

---
+++ src/main/java/de/nekosarekawaii/vandalism/injection/mixins/fix/minecraft/MixinMultiplayerServerListWidget.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of Vandalism - https://github.com/VandalismDevelopment/Vandalism
+ * Copyright (C) 2023-2024 NekosAreKawaii, Verschlxfene, FooFieOwO and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.nekosarekawaii.vandalism.injection.mixins.fix.minecraft;
+
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
+import net.minecraft.client.option.ServerList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Mixin(MultiplayerServerListWidget.class)
+public abstract class MixinMultiplayerServerListWidget {
+
+    @Shadow
+    public List<MultiplayerServerListWidget.ServerEntry> servers;
+
+    @Inject(method = "setServers", at = @At("HEAD"))
+    private void fixCCME(ServerList servers, CallbackInfo ci) {
+        this.servers = new CopyOnWriteArrayList<>();
+    }
+
+}
