50428cbff15095141e5075242f8ea4633a316e85
richard <mail@richard.yt>
2024-03-09 06:26:21 +0100

    migrate items to packageStack util

--- src/main/java/de/nekosarekawaii/vandalism/feature/creativetab/impl/CrashItemsCreativeTab.java
+++ src/main/java/de/nekosarekawaii/vandalism/feature/creativetab/impl/CrashItemsCreativeTab.java
@@ -21,6 +21,7 @@ package de.nekosarekawaii.vandalism.feature.creativetab.impl;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import de.nekosarekawaii.vandalism.feature.creativetab.AbstractCreativeTab;
+import de.nekosarekawaii.vandalism.util.game.ItemStackUtil;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.*;
@@ -83,24 +84,15 @@ public class CrashItemsCreativeTab extends AbstractCreativeTab {
     }
 
     private static ItemStack createClientInstantCrashSkullV3() {
-        final ItemStack item = new ItemStack(Items.HOPPER);
+        final ItemStack item = new ItemStack(Items.PLAYER_HEAD);
         final NbtCompound base = new NbtCompound();
-        final NbtCompound blockEntityTag = new NbtCompound();
-        final NbtList items = new NbtList();
-        final NbtCompound firstSlot = new NbtCompound();
-        firstSlot.putByte("Slot", (byte) 0);
-        firstSlot.putString("id", "skull");
-        firstSlot.putByte("Count", (byte) 1);
         final NbtCompound skullOwner = new NbtCompound();
         final NbtCompound id = new NbtCompound();
         id.putString("Id", "0");
         skullOwner.put("SkullOwner", id);
-        firstSlot.put("tag", skullOwner);
-        items.add(firstSlot);
-        blockEntityTag.put("Items", items);
-        base.put("BlockEntityTag", blockEntityTag);
+        base.put("tag", skullOwner);
         item.setNbt(base);
-        return item;
+        return ItemStackUtil.packageStack(item, ItemStackUtil.PackageType.HOPPER);
     }
 
     private static ItemStack createClientInstantCrashSign(final String component) {
