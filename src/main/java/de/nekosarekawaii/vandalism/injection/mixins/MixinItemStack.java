0419149f3c76b1ea806895a869aaa738b8aa4ed3
Lucy Luna <117228413+cuddlycatgirl@users.noreply.github.com>
2024-04-24 15:56:40 +0200

    Commented out NBT stuff

--- src/main/java/de/nekosarekawaii/vandalism/injection/mixins/MixinItemStack.java
+++ src/main/java/de/nekosarekawaii/vandalism/injection/mixins/MixinItemStack.java
@@ -18,16 +18,13 @@
 
 package de.nekosarekawaii.vandalism.injection.mixins;
 
-import de.nekosarekawaii.vandalism.integration.creativetab.CreativeTabManager;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ItemStack.class)
 public abstract class MixinItemStack {
 
+    // TODO fix
   /*  @Redirect(method = "hasGlint", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item;hasGlint(Lnet/minecraft/item/ItemStack;)Z"))
     private boolean applyClientsideGlint(final Item instance, final ItemStack stack) {
         final var nbt = stack.getNbt();
