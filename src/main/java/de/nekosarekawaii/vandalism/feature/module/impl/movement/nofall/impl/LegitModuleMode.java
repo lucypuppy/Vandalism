bcacb629936ff711faa8b3be6beee509bf7f2924
recyz <aze.hacks002@gmail.com>
2024-05-01 10:42:16 +0200

    Added LegitModuleMode (water mlg) to NoFallModule

---
+++ src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/movement/nofall/impl/LegitModuleMode.java
@@ -0,0 +1,76 @@
+/*
+ * This file is part of Vandalism - https://github.com/VandalismDevelopment/Vandalism
+ * Copyright (C) 2023-2024 NekosAreKawaii, FooFieOwO and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.nekosarekawaii.vandalism.feature.module.impl.movement.nofall.impl;
+
+import de.nekosarekawaii.vandalism.Vandalism;
+import de.nekosarekawaii.vandalism.event.normal.player.PlayerUpdateListener;
+import de.nekosarekawaii.vandalism.event.normal.player.RotationListener;
+import de.nekosarekawaii.vandalism.feature.module.impl.movement.nofall.NoFallModule;
+import de.nekosarekawaii.vandalism.feature.module.template.ModuleMulti;
+import de.nekosarekawaii.vandalism.integration.newrotation.Rotation;
+import de.nekosarekawaii.vandalism.util.game.ChatUtil;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+
+public class LegitModuleMode extends ModuleMulti<NoFallModule> implements PlayerUpdateListener, RotationListener {
+
+    private Rotation rotation = null;
+
+    public LegitModuleMode() {
+        super("Legit");
+    }
+
+    @Override
+    public void onActivate() {
+        Vandalism.getInstance().getEventSystem().subscribe(this, PlayerUpdateEvent.ID, RotationEvent.ID);
+        ChatUtil.chatMessage("You need to have a water bucket in your hotbar to use this mode.");
+    }
+
+    @Override
+    public void onDeactivate() {
+        Vandalism.getInstance().getEventSystem().unsubscribe(this, PlayerUpdateEvent.ID, RotationEvent.ID);
+        Vandalism.getInstance().getRotationManager().resetRotation();
+    }
+
+    @Override
+    public void onPrePlayerUpdate(PlayerUpdateEvent event) {
+        if(mc.player.fallDistance < 3 || mc.world.getBlockState(mc.player.getBlockPos().down()).isAir() || !mc.player.getInventory().contains(new ItemStack(Items.WATER_BUCKET))) {
+            return;
+        }
+        for(int i = 0; i <= 9; i++) {
+            if(mc.player.getInventory().getStack(i).getItem() == Items.WATER_BUCKET) {
+                mc.player.getInventory().selectedSlot = i;
+                break;
+            }
+        }
+        if(mc.player.getMainHandStack().getItem() == Items.WATER_BUCKET) {
+            mc.doItemUse();
+        }
+    }
+
+    @Override
+    public void onRotation(RotationEvent event) {
+        if(mc.player.fallDistance < 3 || !mc.player.getInventory().contains(new ItemStack(Items.WATER_BUCKET))) {
+            Vandalism.getInstance().getRotationManager().resetRotation();
+            return;
+        }
+        this.rotation = new Rotation(mc.player.getYaw(), 90);
+        Vandalism.getInstance().getRotationManager().setRotation(this.rotation, 80f, 0.0f, true);
+    }
+}
