2bf2cd3950b86031f57c0182ac56da9030b1d326
Lucy <admin@catgirls.re>
2024-05-03 13:13:34 +0200

    Added TimerRangeModule
    Co-authored-by: recyz <aze.hacks002@gmail.com>

---
+++ src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/combat/TimerRangeModule.java
@@ -0,0 +1,64 @@
+/*
+ * This file is part of Vandalism - https://github.com/VandalismDevelopment/Vandalism
+ * Copyright (C) 2023-2024 NekosAreKawaii, FooFieOwO and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.nekosarekawaii.vandalism.feature.module.impl.combat;
+
+import de.nekosarekawaii.vandalism.Vandalism;
+import de.nekosarekawaii.vandalism.event.normal.game.TickTimeListener;
+import de.nekosarekawaii.vandalism.feature.module.AbstractModule;
+
+public class TimerRangeModule extends AbstractModule implements TickTimeListener {
+
+    public TimerRangeModule() {
+        super("Timer Range", "Automatically speeds up the speed of the game.", Category.COMBAT);
+    }
+
+    private boolean hurt;
+    private int ticks;
+
+    @Override
+    public void onActivate() {
+        Vandalism.getInstance().getEventSystem().subscribe(this, TickTimeEvent.ID);
+    }
+
+    @Override
+    public void onDeactivate() {
+        hurt = false;
+        ticks = 0;
+        Vandalism.getInstance().getEventSystem().unsubscribe(this, TickTimeEvent.ID);
+    }
+
+    @Override
+    public void onTickTimings(TickTimeEvent event) {
+        if (mc.player == null) return;
+
+        if (mc.player.hurtTime > 9) {
+            event.fromPercentage(0.5f);
+            hurt = true;
+        } else if (mc.player.hurtTime <= 4) {
+            if (hurt) {
+                event.fromPercentage(2f);
+
+                if (ticks >= 6) {
+                    ticks = 0;
+                    hurt = false;
+                } else ticks++;
+            } else event.fromPercentage(1.0f);
+        } else event.fromPercentage(0.75f);
+    }
+}
