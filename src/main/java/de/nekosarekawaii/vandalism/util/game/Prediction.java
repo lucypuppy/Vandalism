21c31cf1ae003c564141c62b9e01ed68b3f9b752
FooFieOwO <foofieowo@gmail.com>
2024-05-13 15:36:03 +0200

    test prediction

---
+++ src/main/java/de/nekosarekawaii/vandalism/util/game/Prediction.java
@@ -0,0 +1,64 @@
+/*
+ * This file is part of Vandalism - https://github.com/VandalismDevelopment/Vandalism
+ * Copyright (C) 2023-2024 NekosAreKawaii, FooFieOwO and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.nekosarekawaii.vandalism.util.game;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Arm;
+import net.minecraft.util.math.Vec3d;
+
+public class Prediction {
+
+    public Vec3d predictEntityMovement(LivingEntity entity, int ticks) {
+        final LivingEntity livingEntity = new LivingEntity((EntityType<? extends LivingEntity>) entity.getType(), entity.getWorld()) {
+            @Override
+            public Iterable<ItemStack> getArmorItems() {
+                return entity.getArmorItems();
+            }
+
+            @Override
+            public ItemStack getEquippedStack(EquipmentSlot slot) {
+                return entity.getEquippedStack(slot);
+            }
+
+            @Override
+            public void equipStack(EquipmentSlot slot, ItemStack stack) {
+                // Do nothing
+            }
+
+            @Override
+            public Arm getMainArm() {
+                return entity.getMainArm();
+            }
+        };
+
+        livingEntity.copyFrom(entity);
+        livingEntity.copyPositionAndRotation(entity);
+        livingEntity.setVelocity(entity.getVelocity());
+
+        for (int i = 0; i < ticks; i++) {
+            livingEntity.tick();
+        }
+
+        return livingEntity.getPos();
+    }
+
+}
