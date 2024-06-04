a105e7ed7748ba8ff35de5eb4edeb41278dd707b
NekosAreKawaii <admin@nekosarekawaii.de>
2024-06-04 15:36:18 +0200

    SmartVClip code cleanup

--- src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/movement/SmartVClip.java
+++ src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/movement/SmartVClip.java
@@ -30,17 +30,12 @@ import org.lwjgl.glfw.GLFW;
 
 public class SmartVClip extends AbstractModule implements KeyboardInputListener {
 
-    private final KeyBindValue upKey = new KeyBindValue(this, "Up Key",
-            "The key that needs to be pressed to go upward", GLFW.GLFW_KEY_UP);
-
-    private final KeyBindValue downKey = new KeyBindValue(this, "Down Key",
-            "The key that needs to be pressed to go downward", GLFW.GLFW_KEY_DOWN);
-
-    private final IntegerValue maxScanRange = new IntegerValue(this, "Max Scan Range",
-            "The maximum range to port to", 9, 3, 100);
+    private final KeyBindValue upKey = new KeyBindValue(this, "Up Key", "The key that needs to be pressed to go upward.", GLFW.GLFW_KEY_UP);
+    private final KeyBindValue downKey = new KeyBindValue(this, "Down Key", "The key that needs to be pressed to go downward.", GLFW.GLFW_KEY_DOWN);
+    private final IntegerValue maxScanRange = new IntegerValue(this, "Max Scan Range", "The maximum range to port to.", 9, 3, 100);
 
     public SmartVClip() {
-        super("SmartVClip", "A automatic vclip module", Category.MOVEMENT);
+        super("Smart V-Clip", "A automatic v-clip module.", Category.MOVEMENT);
     }
 
     @Override
@@ -54,28 +49,29 @@ public class SmartVClip extends AbstractModule implements KeyboardInputListener 
     }
 
     @Override
-    public void onKeyInput(long window, int key, int scanCode, int action, int modifiers) {
-        if (action != GLFW.GLFW_PRESS)
+    public void onKeyInput(final long window, final int key, final int scanCode, final int action, final int modifiers) {
+        if (action != GLFW.GLFW_PRESS) {
             return;
+        }
 
-        if (upKey.isPressed() || downKey.isPressed()) {
-            final BlockPos oldPos = mc.player.getBlockPos().mutableCopy();
+        if (this.upKey.isPressed() || this.downKey.isPressed()) {
+            final BlockPos oldPos = this.mc.player.getBlockPos().mutableCopy();
 
-            for (int i = 1; i < maxScanRange.getValue(); i++) {
-                final BlockPos safeBlockPos = oldPos.add(0, upKey.isPressed() ? i : (-(i + 1)), 0);
+            for (int i = 1; i < this.maxScanRange.getValue(); i++) {
+                final BlockPos safeBlockPos = oldPos.add(0, this.upKey.isPressed() ? i : (-(i + 1)), 0);
                 final BlockPos airBlock1 = safeBlockPos.add(0, 1, 0);
                 final BlockPos airBlock2 = safeBlockPos.add(0, 2, 0);
 
-                final BlockState safeBlock = mc.world.getBlockState(safeBlockPos);
-                final BlockState airBlock1State = mc.world.getBlockState(airBlock1);
-                final BlockState airBlock2State = mc.world.getBlockState(airBlock2);
+                final BlockState safeBlock = this.mc.world.getBlockState(safeBlockPos);
+                final BlockState airBlock1State = this.mc.world.getBlockState(airBlock1);
+                final BlockState airBlock2State = this.mc.world.getBlockState(airBlock2);
 
-                if (safeBlock.isSolidSurface(mc.world, safeBlockPos, mc.player, Direction.UP) &&
-                        airBlock1State.isAir() && airBlock2State.isAir()) {
-                    mc.player.updatePosition(safeBlockPos.getX() + 0.5, safeBlockPos.getY() + 1, safeBlockPos.getZ() + 0.5);
+                if (safeBlock.isSolidSurface(this.mc.world, safeBlockPos, this.mc.player, Direction.UP) && airBlock1State.isAir() && airBlock2State.isAir()) {
+                    this.mc.player.updatePosition(safeBlockPos.getX() + 0.5, safeBlockPos.getY() + 1, safeBlockPos.getZ() + 0.5);
                     break;
                 }
             }
         }
     }
+
 }
