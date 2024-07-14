2371abec69e4aabd1b5d6bfc443e2ff059f949b8
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2024-07-14 14:08:53 +0200

    Added matrix glide

---
+++ src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/movement/flight/impl/MatrixModuleMode.java
@@ -0,0 +1,59 @@
+/*
+ * This file is part of Vandalism - https://github.com/NekosAreKawaii/Vandalism
+ * Copyright (C) 2023-2024 NekosAreKawaii, FooFieOwO, Verschlxfene, Recyz and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.nekosarekawaii.vandalism.feature.module.impl.movement.flight.impl;
+
+import de.nekosarekawaii.vandalism.Vandalism;
+import de.nekosarekawaii.vandalism.event.game.TickTimeListener;
+import de.nekosarekawaii.vandalism.event.player.PlayerUpdateListener;
+import de.nekosarekawaii.vandalism.feature.module.impl.movement.flight.FlightModule;
+import de.nekosarekawaii.vandalism.feature.module.template.module.ModuleMulti;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MatrixModuleMode extends ModuleMulti<FlightModule> implements PlayerUpdateListener, TickTimeListener {
+
+    public MatrixModuleMode() {
+        super("Matrix");
+    }
+
+    @Override
+    public void onActivate() {
+        Vandalism.getInstance().getEventSystem().subscribe(this, PlayerUpdateEvent.ID, TickTimeEvent.ID);
+    }
+
+    @Override
+    public void onDeactivate() {
+        Vandalism.getInstance().getEventSystem().unsubscribe(this, PlayerUpdateEvent.ID, TickTimeEvent.ID);
+    }
+
+    @Override
+    public void onPrePlayerUpdate(final PlayerUpdateEvent event) {
+        if (this.mc.player.age % 5 == 0) {
+            final Vec3d velocity = this.mc.player.getVelocity();
+            this.mc.player.setVelocity(velocity.x, 0, velocity.z);
+        }
+    }
+
+    @Override
+    public void onTickTimings(final TickTimeEvent event) {
+        event.fromPercentage(ThreadLocalRandom.current().nextFloat(0.1f, 0.2f));
+    }
+
+}
