74dc64ad7c68089a7e15cabe2be64965c217827d
nellieldev <nellieldev@gmail.com>
2024-08-20 03:55:37 +0200

    Added kaboom kick command

---
+++ src/main/java/de/nekosarekawaii/vandalism/feature/command/arguments/GreedyPlayerArgumentType.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of Vandalism - https://github.com/NekosAreKawaii/Vandalism
+ * Copyright (C) 2023-2024 NekosAreKawaii, FooFieOwO, Recyz and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.nekosarekawaii.vandalism.feature.command.arguments;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import de.nekosarekawaii.vandalism.util.MinecraftWrapper;
+import net.minecraft.command.CommandSource;
+
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+
+public class GreedyPlayerArgumentType implements ArgumentType<String>, MinecraftWrapper {
+
+    public static GreedyPlayerArgumentType create() {
+        return new GreedyPlayerArgumentType();
+    }
+
+    public static String get(final CommandContext<?> context) {
+        return context.getArgument("name", String.class);
+    }
+
+    @Override
+    public String parse(final StringReader reader) throws CommandSyntaxException {
+        final String text = reader.getRemaining();
+        reader.setCursor(reader.getTotalLength());
+        return text;
+    }
+
+    @Override
+    public <S> CompletableFuture<Suggestions> listSuggestions(final CommandContext<S> context, final SuggestionsBuilder builder) {
+        return CommandSource.suggestMatching(Objects.requireNonNull(this.mc.getNetworkHandler()).getPlayerList().stream().map(playerListEntry -> playerListEntry.getProfile().getName()), builder);
+    }
+
+}
--- src/main/java/de/nekosarekawaii/vandalism/injection/mixins/module/MixinClientPlayNetworkAddon.java
+++ src/main/java/de/nekosarekawaii/vandalism/injection/mixins/module/MixinClientPlayNetworkAddon.java
@@ -29,6 +29,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = ClientPlayNetworkAddon.class, remap = false)
 public abstract class MixinClientPlayNetworkAddon {
 
+
     @Inject(method = {"invokeRegisterEvent", "invokeUnregisterEvent", "onServerReady"}, at = @At("HEAD"), cancellable = true)
     private void hookModPacketBlocker(final CallbackInfo ci) {
         final ModPacketBlockerModule modPacketBlockerModule = Vandalism.getInstance().getModuleManager().getModPacketBlockerModule();
