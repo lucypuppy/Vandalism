f104f05418e7b501e1fffe2d3d8ad91f41bdd8a4
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2024-09-02 19:19:13 +0200

    Improved help command again

--- src/main/java/de/nekosarekawaii/vandalism/feature/command/impl/misc/HelpCommand.java
+++ src/main/java/de/nekosarekawaii/vandalism/feature/command/impl/misc/HelpCommand.java
@@ -24,6 +24,7 @@ import de.nekosarekawaii.vandalism.Vandalism;
 import de.nekosarekawaii.vandalism.feature.command.AbstractCommand;
 import de.nekosarekawaii.vandalism.util.ChatUtil;
 import net.minecraft.command.CommandSource;
+import net.minecraft.text.ClickEvent;
 import net.minecraft.text.HoverEvent;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
@@ -36,6 +37,7 @@ public class HelpCommand extends AbstractCommand {
                 "Shows information about all available commands.",
                 Category.MISC,
                 "help",
+                "commandlist",
                 "commands",
                 "cmds",
                 "?"
@@ -54,25 +56,25 @@ public class HelpCommand extends AbstractCommand {
         int maxPages = (int) Math.ceil((double) totalCommands / maxCommandsPerPage) - 1;
         int page = Math.max(0, Math.min(pageInput - 1, maxPages));
         ChatUtil.emptyChatMessage(false);
-        final StringBuilder commandsFromTo = new StringBuilder();
-        commandsFromTo.append(Formatting.DARK_AQUA);
-        commandsFromTo.append((page * maxCommandsPerPage) + 1);
-        commandsFromTo.append(Formatting.GRAY).append(" - ");
-        commandsFromTo.append(Formatting.DARK_AQUA);
-        final int endCommand = Math.min((page + 1) * maxCommandsPerPage, totalCommands);
-        commandsFromTo.append(endCommand);
         ChatUtil.chatMessage(Text.literal(
                 Formatting.DARK_GRAY + "[" + Formatting.GOLD + "Page " +
                         Formatting.DARK_AQUA + (page + 1) + Formatting.GRAY + " / " + Formatting.DARK_AQUA + (maxPages + 1) +
-                        Formatting.DARK_GRAY + " | " + Formatting.GOLD + "Command " + commandsFromTo + Formatting.GRAY + " / " + Formatting.DARK_AQUA + totalCommands + Formatting.DARK_GRAY + "]"
+                        Formatting.DARK_GRAY + " | " + Formatting.GOLD + "Commands" + Formatting.GRAY + ": " + Formatting.DARK_AQUA + totalCommands + Formatting.DARK_GRAY + "]"
         ));
         ChatUtil.emptyChatMessage(false);
         final String commandPrefix = Vandalism.getInstance().getClientSettings().getChatSettings().commandPrefix.getValue();
-        for (int i = page * maxCommandsPerPage; i < endCommand; i++) {
+        for (int i = page * maxCommandsPerPage; i < Math.min((page + 1) * maxCommandsPerPage, totalCommands); i++) {
             final AbstractCommand command = Vandalism.getInstance().getCommandManager().getList().get(i);
             final MutableText commandText = Text.literal(commandPrefix + String.join(" | ", command.getAliases()));
             commandText.formatted(Formatting.YELLOW);
             commandText.append(Text.literal(" > ").formatted(Formatting.DARK_GRAY));
+            commandText.styled(style ->
+                    style.withClickEvent(new ClickEvent(
+                            ClickEvent.Action.SUGGEST_COMMAND, commandPrefix + command.getAliases()[0]
+                    )).withHoverEvent(new HoverEvent(
+                            HoverEvent.Action.SHOW_TEXT, Text.literal("Insert the command into the chat field")
+                    ))
+            );
             String description = command.getDescription();
             if (description == null || description.isEmpty()) {
                 description = "No description available.";
@@ -91,7 +93,7 @@ public class HelpCommand extends AbstractCommand {
                     .styled(style -> style
                             .withClickEvent(Vandalism.getInstance().getCommandManager().generateClickEvent("help " + prevPage))
                             .withHoverEvent(new HoverEvent(
-                                    HoverEvent.Action.SHOW_TEXT, Text.literal("Go to page " + prevPage + "."))
+                                    HoverEvent.Action.SHOW_TEXT, Text.literal("Go to page " + prevPage))
                             )
                     );
             buttons.append(prevPageButton);
@@ -105,7 +107,7 @@ public class HelpCommand extends AbstractCommand {
                     .styled(style -> style
                             .withClickEvent(Vandalism.getInstance().getCommandManager().generateClickEvent("help " + nextPage))
                             .withHoverEvent(new HoverEvent(
-                                    HoverEvent.Action.SHOW_TEXT, Text.literal("Go to page " + nextPage + "."))
+                                    HoverEvent.Action.SHOW_TEXT, Text.literal("Go to page " + nextPage))
                             )
                     );
             buttons.append(nextPageButton);
