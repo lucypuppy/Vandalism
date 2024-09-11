819441d18c7bd6499749c1cc72e7bd18ac3220be
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2024-09-11 16:25:09 +0200

    Added unfocused fps module

---
+++ src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/render/UnfocusedFPSModule.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of Vandalism - https://github.com/NekosAreKawaii/Vandalism
+ * Copyright (C) 2023-2024 NekosAreKawaii, FooFieOwO, Recyz and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.nekosarekawaii.vandalism.feature.module.impl.render;
+
+import de.nekosarekawaii.vandalism.base.value.impl.number.IntegerValue;
+import de.nekosarekawaii.vandalism.feature.module.AbstractModule;
+
+public class UnfocusedFPSModule extends AbstractModule {
+
+    public final IntegerValue maxFPS = new IntegerValue(
+            this,
+            "Max FPS",
+            "The max fps when the window isn't focused.",
+            10,
+            1,
+            30
+    );
+
+    public UnfocusedFPSModule() {
+        super(
+                "Unfocused FPS",
+                "Decreases the max fps of the game when the window isn't focused.",
+                Category.RENDER
+        );
+    }
+
+}
