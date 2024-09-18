9d25cf9416329706f6967cd68f89e6eb3fa0966e
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2024-09-19 00:31:23 +0200

    Turned unfocused fps into a visual setting (default on)

--- src/main/java/de/nekosarekawaii/vandalism/injection/mixins/clientsetting/MixinMinecraftClient.java
+++ src/main/java/de/nekosarekawaii/vandalism/injection/mixins/clientsetting/MixinMinecraftClient.java
@@ -16,37 +16,36 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package de.nekosarekawaii.vandalism.injection.mixins.module;
+package de.nekosarekawaii.vandalism.injection.mixins.clientsetting;
 
 import de.nekosarekawaii.vandalism.Vandalism;
-import de.nekosarekawaii.vandalism.feature.module.impl.misc.FastPlaceModule;
-import de.nekosarekawaii.vandalism.feature.module.impl.render.ESPModule;
+import de.nekosarekawaii.vandalism.base.clientsettings.impl.VisualSettings;
 import de.nekosarekawaii.vandalism.util.MinecraftWrapper;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.Entity;
+import net.minecraft.client.option.GameOptions;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = MinecraftClient.class)
 public abstract class MixinMinecraftClient implements MinecraftWrapper {
 
-    @Inject(method = "hasOutline", at = @At("RETURN"), cancellable = true)
-    private void hookESP(final Entity entity, final CallbackInfoReturnable<Boolean> cir) {
-        final ESPModule espModule = Vandalism.getInstance().getModuleManager().getEspModule();
-        if (entity != this.mc.player && espModule.isActive() && espModule.isTarget(entity)) {
-            cir.setReturnValue(true);
-        }
-    }
+    @Shadow
+    public abstract boolean isWindowFocused();
 
-    @ModifyConstant(method = "doItemUse", constant = @Constant(intValue = 4))
-    private int hookFastPlace(final int value) {
-        final FastPlaceModule fastPlaceModule = Vandalism.getInstance().getModuleManager().getFastPlaceModule();
-        if (fastPlaceModule.isActive()) return fastPlaceModule.cooldown.getValue();
-        return value;
+    @Shadow
+    @Final
+    public GameOptions options;
+
+    @Inject(method = "getFramerateLimit", at = @At("HEAD"), cancellable = true)
+    private void unfocusedFPS(final CallbackInfoReturnable<Integer> info) {
+        final VisualSettings visualSettings = Vandalism.getInstance().getClientSettings().getVisualSettings();
+        if (visualSettings.unfocusedFPS.getValue() && !isWindowFocused()) {
+            info.setReturnValue(Math.min(visualSettings.maxUnfocusedFPS.getValue(), this.options.getMaxFps().getValue()));
+        }
     }
 
 }
