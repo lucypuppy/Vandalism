2a995e02275b6e526668b7bc32baa45f8899be20
Thomas <notyourreturn@gmail.com>
2024-10-05 20:59:44 +0200

    BIG TELEPORT IMPROVEMENT

--- src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/movement/teleport/TeleportModule.java
+++ src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/movement/teleport/TeleportModule.java
@@ -19,11 +19,16 @@
 package de.nekosarekawaii.vandalism.feature.module.impl.movement.teleport;
 
 import de.nekosarekawaii.vandalism.Vandalism;
-import de.nekosarekawaii.vandalism.base.value.impl.number.FloatValue;
+import de.nekosarekawaii.vandalism.base.value.impl.number.IntegerValue;
+import de.nekosarekawaii.vandalism.event.game.KeyboardInputListener;
+import de.nekosarekawaii.vandalism.event.player.PlayerUpdateListener;
 import de.nekosarekawaii.vandalism.event.render.Render3DListener;
 import de.nekosarekawaii.vandalism.feature.module.Module;
 import de.nekosarekawaii.vandalism.feature.module.impl.movement.teleport.impl.VanillaModuleMode;
 import de.nekosarekawaii.vandalism.feature.module.template.module.ModuleModeValue;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.debug.DebugRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -31,21 +36,30 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import org.lwjgl.glfw.GLFW;
 
-public class TeleportModule extends Module implements Render3DListener {
+import java.util.Arrays;
+import java.util.List;
 
-    public final FloatValue maxDistance = new FloatValue(
+public class TeleportModule extends Module implements Render3DListener, PlayerUpdateListener, KeyboardInputListener {
+
+    public boolean teleport;
+    public Vec3d selectedPos;
+    private BlockPos hoverPos;
+    private final List<Block> blackList = Arrays.asList(Blocks.AIR, Blocks.CAVE_AIR, Blocks.VOID_AIR);
+
+    public final IntegerValue maxDistance = new IntegerValue(
             this,
             "Max Distance",
             "Max teleport distance.",
-            10.0f,
-            0.0f,
-            100.0f
+            100,
+            10,
+            250
     );
 
     public final ModuleModeValue<TeleportModule> mode = new ModuleModeValue<>(this, "Mode", "The mode of the teleport.",
             new VanillaModuleMode(this)
-            //new VulcanModuleMode(this) TODO: Fix vulcan teleport
+            //new VulcanModuleMode(this) TODO: (Not needed Vanilla bypasses Vulcan) Fix vulcan teleport
     );
 
     public TeleportModule() {
@@ -58,43 +72,80 @@ public class TeleportModule extends Module implements Render3DListener {
 
     @Override
     public void onActivate() {
-        Vandalism.getInstance().getEventSystem().subscribe(this, Render3DEvent.ID);
+        Vandalism.getInstance().getEventSystem().subscribe(this, PlayerUpdateEvent.ID, KeyboardInputEvent.ID, Render3DEvent.ID);
     }
 
     @Override
     public void onDeactivate() {
-        Vandalism.getInstance().getEventSystem().unsubscribe(this, Render3DEvent.ID);
+        Vandalism.getInstance().getEventSystem().unsubscribe(this, PlayerUpdateEvent.ID, KeyboardInputEvent.ID, Render3DEvent.ID);
     }
 
-    public boolean canTeleport() {
-        return this.mc.player.isUsingItem() || this.mc.options.useKey.isPressed();
-    }
-
-    public Vec3d getBlockHitResult() {
+    private BlockPos getBlockHitResult() {
         final HitResult result = this.mc.player.raycast(this.maxDistance.getValue(), this.mc.getRenderTickCounter().getTickDelta(false), false);
-        if (result instanceof BlockHitResult) {
-            return result.getPos();
+        if (result instanceof BlockHitResult blockHitResult) {
+            Block block = this.mc.world.getBlockState(blockHitResult.getBlockPos()).getBlock();
+            if (!this.blackList.contains(block)) {
+                return blockHitResult.getBlockPos();
+            }
         }
         return null;
     }
 
     @Override
+    public void onKeyInput(long window, int key, int scanCode, int action, int modifiers) {
+        if (this.selectedPos != null && key == GLFW.GLFW_KEY_LEFT_SHIFT) {
+            this.teleport = true;
+        }
+    }
+
+    @Override
+    public void onPrePlayerUpdate(PlayerUpdateEvent event) {
+        if (GLFW.glfwGetMouseButton(this.mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_RIGHT) == 1 && this.hoverPos != null) {
+            this.selectedPos = this.hoverPos.toCenterPos();
+        }
+        this.hoverPos = this.getBlockHitResult();
+    }
+
+    @Override
     public void onRender3D(final float tickDelta, final MatrixStack matrixStack) {
-        final Vec3d target = this.getBlockHitResult();
-        if (target == null) return;
-        final BlockPos pos = new BlockPos((int) target.getX(), (int) target.getY(), (int) target.getZ());
         final VertexConsumerProvider.Immediate immediate = this.mc.getBufferBuilders().getEntityVertexConsumers();
+        Vec3d vec = MinecraftClient.getInstance().gameRenderer.getCamera().getPos().negate();
         matrixStack.push();
-        float[] color = new float[]{1f, 0f, 0f};
-        DebugRenderer.drawBox(
-                matrixStack,
-                immediate,
-                pos,
-                pos,
-                color[0], color[1], color[2], 0.5f
-        );
-        matrixStack.pop();
+        matrixStack.translate(vec.x, vec.y, vec.z);
+        if (this.hoverPos != null && !this.hoverPos.toCenterPos().equals(this.selectedPos)) {
+            DebugRenderer.drawBox(
+                    matrixStack,
+                    immediate,
+                    this.hoverPos.getX(),
+                    this.hoverPos.getY(),
+                    this.hoverPos.getZ(),
+                    this.hoverPos.getX() + 1,
+                    this.hoverPos.getY() + 1,
+                    this.hoverPos.getZ() + 1,
+                    1F,
+                    0F,
+                    0F,
+                    0.5F
+            );
+        }
+        if (this.selectedPos != null) {
+            DebugRenderer.drawBox(
+                    matrixStack,
+                    immediate,
+                    this.selectedPos.getX() - 0.5,
+                    this.selectedPos.getY() - 0.5,
+                    this.selectedPos.getZ() - 0.5,
+                    this.selectedPos.getX() + 0.5,
+                    this.selectedPos.getY() + 0.5,
+                    this.selectedPos.getZ() + 0.5,
+                    0F,
+                    1F,
+                    0F,
+                    0.5F
+            );
+        }
         immediate.draw();
+        matrixStack.pop();
     }
 
 }
