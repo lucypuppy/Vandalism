10cb93d414044b8afb2c1062236d2d3cb4350186
recyz <aze.hacks002@gmail.com>
2024-10-12 20:49:24 +0200

    Added new CubeCraft nofall (not final atm will improve it)

--- src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/movement/nofall/impl/CubeCraftModuleMode.java
+++ src/main/java/de/nekosarekawaii/vandalism/feature/module/impl/movement/nofall/impl/CubeCraftModuleMode.java
@@ -19,39 +19,44 @@
 package de.nekosarekawaii.vandalism.feature.module.impl.movement.nofall.impl;
 
 import de.nekosarekawaii.vandalism.Vandalism;
-import de.nekosarekawaii.vandalism.event.network.OutgoingPacketListener;
+import de.nekosarekawaii.vandalism.event.game.GameTickListener;
 import de.nekosarekawaii.vandalism.feature.module.impl.movement.nofall.NoFallModule;
 import de.nekosarekawaii.vandalism.feature.module.template.module.ModuleMulti;
-import de.nekosarekawaii.vandalism.util.MinecraftConstants;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.block.AirBlock;
 
-public class CubeCraftModuleMode extends ModuleMulti<NoFallModule> implements OutgoingPacketListener {
+public class CubeCraftModuleMode extends ModuleMulti<NoFallModule> implements GameTickListener {
 
     public CubeCraftModuleMode() {
-        super("CubeCraft Ground");
+        super("CubeCraft");
     }
 
     @Override
     public void onActivate() {
-        Vandalism.getInstance().getEventSystem().subscribe(OutgoingPacketEvent.ID, this);
+        Vandalism.getInstance().getEventSystem().subscribe(GameTickEvent.ID, this);
     }
 
     @Override
     public void onDeactivate() {
-        Vandalism.getInstance().getEventSystem().unsubscribe(OutgoingPacketEvent.ID, this);
+        Vandalism.getInstance().getEventSystem().unsubscribe(GameTickEvent.ID, this);
     }
 
     @Override
-    public void onOutgoingPacket(final OutgoingPacketEvent event) {
-        if (event.packet instanceof final PlayerMoveC2SPacket playerPacket
-                && this.mc.player.fallDistance > 3f) {
-            double next = this.mc.player.getY() % MinecraftConstants.MAGIC_ON_GROUND_MODULO_FACTOR;
-            next -= (int) this.mc.player.getY();
-            next %= MinecraftConstants.MAGIC_ON_GROUND_MODULO_FACTOR;
-            double y = this.mc.player.getY() - next;
-            playerPacket.y = y;
-            playerPacket.onGround = true;
+    public void onGameTick(GameTickEvent event) {
+        if (mc.player == null || mc.world == null) {
+            return;
+        }
+
+        if (mc.player.fallDistance > 3) {
+            for (int i = 1; i <= 4; i++) {
+                if (!(mc.world.getBlockState(mc.player.getBlockPos().down(i)).getBlock() instanceof AirBlock)) {
+                    if (i == 4) {
+                        mc.player.setVelocity(0, 0, 0);
+                    }
+                    mc.player.setVelocity(0, 0.01, 0);
+                    mc.player.fallDistance = 0;
+                    return;
+                }
+            }
         }
     }
-
 }
