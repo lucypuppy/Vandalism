1ee3e9ed9f1c9e567936f4de41a3ea527e43ecab
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2024-10-14 00:33:26 +0200

    Fixed baritone settings rendering

--- addon-baritone/src/main/java/de/nekosarekawaii/vandalism/addonbaritone/gui/BaritoneSettingsClientWindow.java
+++ addon-baritone/src/main/java/de/nekosarekawaii/vandalism/addonbaritone/gui/BaritoneSettingsClientWindow.java
@@ -22,6 +22,7 @@ import de.nekosarekawaii.vandalism.addonbaritone.settings.BaritoneSettingMapper;
 import de.nekosarekawaii.vandalism.base.value.Value;
 import de.nekosarekawaii.vandalism.clientwindow.base.ClientWindow;
 import imgui.ImGui;
+import imgui.flag.ImGuiCol;
 import imgui.flag.ImGuiWindowFlags;
 import imgui.type.ImString;
 import net.minecraft.client.gui.DrawContext;
@@ -53,13 +54,15 @@ public class BaritoneSettingsClientWindow extends ClientWindow {
         ImGui.setNextItemWidth(-1);
         ImGui.inputText(id + "input", this.searchInput);
         ImGui.separator();
+        ImGui.pushStyleColor(ImGuiCol.ChildBg, 0.0f, 0.0f, 0.0f, 0.0f);
         ImGui.beginChild(id + "scrolllist", -1, -1, true);
         for (final Value<?> value : this.baritoneSettingMapper.getValues()) {
-            if (this.searchInput.isEmpty() || value.getName().toLowerCase().contains(searchInput.get().toLowerCase())) {
+            if (this.searchInput.isEmpty() || value.getName().toLowerCase().contains(this.searchInput.get().toLowerCase())) {
                 this.baritoneSettingMapper.renderValue(value, true);
             }
         }
         ImGui.endChild();
+        ImGui.popStyleColor();
     }
 
 }
