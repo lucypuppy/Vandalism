e067f96c6249406f4e3cd13661cd0c52086ff80e
NekosAreKawaii <admin@nekosarekawaii.de>
2024-10-27 10:55:47 +0100

    Add Mace Insta Kill Module

--- src/main/java/de/nekosarekawaii/vandalism/feature/module/ModuleManager.java
+++ src/main/java/de/nekosarekawaii/vandalism/feature/module/ModuleManager.java
@@ -221,7 +221,8 @@ public class ModuleManager extends NamedStorage<Module> implements
                 new UseItemSpammerModule(),
                 new TriggerBotModule(),
                 new AutoDisconnectModule(),
-                new TeamsModule()
+                new TeamsModule(),
+                new MaceInstaKillModule()
         );
         if (Util.getOperatingSystem() == Util.OperatingSystem.WINDOWS) {
             this.add(new AntiCaptureModule());
