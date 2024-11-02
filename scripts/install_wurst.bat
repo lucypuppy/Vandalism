bc1fac1d3630fb6f9b8cb40dd0f81058527a8ef2
Verschlxfene <152820108+verschlxfene@users.noreply.github.com>
2024-11-02 20:31:41 +0100

    Fix wurst install scripts as well
    Now added an argument for the branch

--- scripts/install_wurst.bat
+++ scripts/install_wurst.bat
@@ -1,16 +1,19 @@
 @echo off
 setlocal
 
+set "BRANCH=%~1"
+if "%BRANCH%"=="" set "BRANCH=master"
+
 cd ..
 
-set REPO_URL=https://github.com/Wurst-Imperium/Wurst7.git
-set REPO_DIR=Wurst7
-set MODS_DIR=run\mods
+set "REPO_URL=https://github.com/Wurst-Imperium/Wurst7.git"
+set "REPO_DIR=Wurst7"
+set "MODS_DIR=run\mods"
 
-if exist %MODS_DIR% (
-    echo Checking for existing JAR files in the binaries directory...
+if exist "%MODS_DIR%" (
+    echo Checking for existing JAR files in the mods directory...
 
-    for %%f in (%MODS_DIR%\*.jar) do (
+    for %%f in ("%MODS_DIR%\*.jar") do (
         echo Found JAR file: %%~nxf
 
         echo %%~nxf | findstr /i /c:"Wurst" >nul
@@ -21,56 +24,46 @@ if exist %MODS_DIR% (
     )
 )
 
-if not exist %REPO_DIR% (
+if not exist "%REPO_DIR%" (
     echo Cloning the repository...
-    git clone %REPO_URL%
+    git clone "%REPO_URL%"
 ) else (
     echo Repository already exists. Pulling latest changes...
-    cd %REPO_DIR%
-    git pull
+    cd "%REPO_DIR%"
+    git checkout "%BRANCH%"
+    git pull origin "%BRANCH%"
     cd ..
 )
 
-:: Read the minecraft_version from gradle.properties
-for /f "tokens=2 delims==" %%a in ('findstr /r /c:"^minecraft_version=" gradle.properties') do set MC_VERSION=%%a
+for /f "tokens=2 delims==" %%a in ('findstr /r /c:"^minecraft_version=" gradle.properties') do set "MC_VERSION=%%a"
 
-cd %REPO_DIR%
-
-:: Switch to the branch corresponding to the minecraft_version
-echo Switching to branch %MC_VERSION%...
-git checkout %MC_VERSION%
+cd "%REPO_DIR%"
 
 echo Building the project...
 call gradlew.bat build
-
 if errorlevel 1 (
     echo Build failed. Exiting...
     exit /b 1
 )
 
-if not exist ..\%MODS_DIR% (
-    mkdir ..\%MODS_DIR%
+if not exist "..\%MODS_DIR%" (
+    mkdir "..\%MODS_DIR%"
 )
 
 echo Copying the JAR file...
+set "JAR_COPIED=0"
 for %%f in (build\libs\*.jar) do (
     echo Checking %%~nxf
     echo %%~nxf | findstr /i /c:"-sources" >nul
     if errorlevel 1 (
         echo Found non-sources JAR: %%~nxf
         copy "%%f" "..\%MODS_DIR%"
-        set JAR_COPIED=1
+        set "JAR_COPIED=1"
     )
 )
 
 cd ..
-
 echo Deleting the cloned repository...
-rmdir /s /q %REPO_DIR%
-
-echo.
-echo When you see an error above this message than say thank you to Gradle,
-echo taskkill java and delete the Wurst7 directory inside the root directory by yourself.
-echo.
+rmdir /s /q "%REPO_DIR%"
 
 echo Done.
--- scripts/install_wurst.sh
+++ scripts/install_wurst.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+BRANCH=${1:-master}
+
 cd ..
 
-REPO_URL=https://github.com/Wurst-Imperium/Wurst7.git
-REPO_DIR=Wurst7
-MODS_DIR=run/mods
+REPO_URL="https://github.com/Wurst-Imperium/Wurst7.git"
+REPO_DIR="Wurst7"
+MODS_DIR="run/mods"
 
 if [ -d "$MODS_DIR" ]; then
     echo "Checking for existing JAR files in the binaries directory..."
 
     for f in "$MODS_DIR"/*.jar; do
-        echo "Found JAR file: $(basename "$f")"
+        if [ -f "$f" ]; then
+            echo "Found JAR file: $(basename "$f")"
 
-        if echo "$(basename "$f")" | grep -iq "Wurst"; then
-            echo "Deleting JAR file: $(basename "$f")"
-            rm "$f"
+            if [[ $(basename "$f") == *Wurst* ]]; then
+                echo "Deleting JAR file: $(basename "$f")"
+                rm "$f"
+            fi
         fi
     done
 fi
@@ -25,35 +29,28 @@ if [ ! -d "$REPO_DIR" ]; then
 else
     echo "Repository already exists. Pulling latest changes..."
     cd "$REPO_DIR"
-    git pull
+    git checkout "$BRANCH"
+    git pull origin "$BRANCH"
     cd ..
 fi
 
-# Read the minecraft_version from gradle.properties
 MC_VERSION=$(grep "^minecraft_version=" gradle.properties | cut -d'=' -f2)
 
 cd "$REPO_DIR"
 
-# Switch to the branch corresponding to the minecraft_version
-echo "Switching to branch $MC_VERSION..."
-git checkout "$MC_VERSION"
-
 echo "Building the project..."
 ./gradlew build
-
 if [ $? -ne 0 ]; then
     echo "Build failed. Exiting..."
     exit 1
 fi
 
-if [ ! -d "../$MODS_DIR" ]; then
-    mkdir -p "../$MODS_DIR"
-fi
+mkdir -p "../$MODS_DIR"
 
 echo "Copying the JAR file..."
+JAR_COPIED=0
 for f in build/libs/*.jar; do
-    echo "Checking $(basename "$f")"
-    if ! echo "$(basename "$f")" | grep -iq "-sources"; then
+    if [[ $(basename "$f") != *-sources.jar ]]; then
         echo "Found non-sources JAR: $(basename "$f")"
         cp "$f" "../$MODS_DIR"
         JAR_COPIED=1
@@ -61,13 +58,7 @@ for f in build/libs/*.jar; do
 done
 
 cd ..
-
 echo "Deleting the cloned repository..."
 rm -rf "$REPO_DIR"
-
-echo
-echo "When you see an error above this message than say thank you to Gradle,"
-echo "kill the java process and delete the Wurst7 directory inside the root directory by yourself."
-echo
 
 echo "Done."
